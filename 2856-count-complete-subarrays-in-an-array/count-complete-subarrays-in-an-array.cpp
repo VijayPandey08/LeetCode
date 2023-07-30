8f3b1217dd0026037d6d3f67f37a7d1498382fc4
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-07-31 02:37:25 +0530

    Time: 2016 ms (60.00%) | Memory: 467 MB (60.00%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -5,20 +5,20 @@ class Solution
         {
             int ans = 0;
             vector<int> v;
-            set<int> s;
+            map<int,int> s;
 
             for (auto i: nums)
             {
-                s.insert(i);
+                s[i]++;
             }
             int size = s.size();
 
             for (int i = 0; i < nums.size(); i++)
             {
-                set<int> s1;
+                map<int,int> s1;
                 for (int j = i; j < nums.size(); j++)
                 {
-                    s1.insert(nums[j]);
+                    s1[nums[j]]++;
                     if (s1.size() == size)
                     {
                         ans = ans + nums.size() - j;
