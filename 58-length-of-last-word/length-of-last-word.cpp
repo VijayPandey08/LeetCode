1985b8ae35c7e408c397caba78fa61915d270304
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-08-02 02:01:23 +0530

    Time: 4 ms (37.97%) | Memory: 6.5 MB (37.27%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+    public:
+        int lengthOfLastWord(string s)
+        {
+           int ans = 0;
+            vector<char> v;
+            cout<<v.size();
+            
+            if(s.length()==1 && s[0]!=' ') return 1;
+            if(s.length()==2 && s[1]==' ') return 1;
+            for (int i = s.length() - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ' && v.size() == 0)
+                {
+                    continue;
+                }
+                if (s[i] != ' ')
+                {
+                    v.push_back(s[i]);
+                }
+                if (s[i] == ' ' && v.size() >0)
+                {
+                    break;
+                }
+            }
+
+            return v.size();
+        }
+};
