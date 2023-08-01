aeb56e8546937d7fb6ea0a52154a9edd61aeb49e
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-08-02 02:01:42 +0530

    Time: 4 ms (37.97%) | Memory: 6.5 MB (37.27%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -7,8 +7,8 @@ class Solution
             vector<char> v;
             cout<<v.size();
             
-            if(s.length()==1 && s[0]!=' ') return 1;
-            if(s.length()==2 && s[1]==' ') return 1;
+            // if(s.length()==1 && s[0]!=' ') return 1;
+            // if(s.length()==2 && s[1]==' ') return 1;
             for (int i = s.length() - 1; i >= 0; i--)
             {
                 if (s[i] == ' ' && v.size() == 0)
