929f4af8d8efce7f002bdadd4cdbec418e5af4f1
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-08-15 01:42:03 +0530

    Time: 6 ms (33.78%) | Memory: 9.7 MB (13.57%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -3,47 +3,47 @@ class Solution
     public:
         int searchInsert(vector<int> &nums, int target)
         {
-            int s = 0;
-            int e = nums.size() - 1;
-            int ans = 0;
+            // int s = 0;
+            // int e = nums.size() - 1;
+            // int ans = 0;
 
-            while (s <= e)
-            {
-                int mid = e - ((e-s)/2);
-                // ans = mid;
-                if (target > nums[mid])
-                {   
-                    s = mid + 1;
-                    ans=s;
+            // while (s <= e)
+            // {
+            //     int mid = e - ((e-s)/2);
+            //     // ans = mid;
+            //     if (target > nums[mid])
+            //     {   
+            //         s = mid + 1;
+            //         ans=s;
                 
-                }
-                else if (target < nums[mid])
-                {   
-                    e = mid - 1;
-                    ans=e;
+            //     }
+            //     else if (target < nums[mid])
+            //     {   
+            //         e = mid - 1;
+            //         ans=e;
                 
-                }
-                else
-                {
-                    return mid;
-                }
-            }
+            //     }
+            //     else
+            //     {
+            //         return mid;
+            //     }
+            // }
 
-            if(ans<0){
-                return 0;
-            }
-            if(ans>nums.size()-1){
-                return ans;
-            }
-            else{
-                if(nums[ans]>target){
-                    return ans;
-                }
-                else{
-                    return ans +1;
-                }
-            }
+            // if(ans<0){
+            //     return 0;
+            // }
+            // if(ans>nums.size()-1){
+            //     return ans;
+            // }
+            // else{
+            //     if(nums[ans]>target){
+            //         return ans;
+            //     }
+            //     else{
+            //         return ans +1;
+            //     }
+            // }
 
-            // return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+            return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
         }
 };
