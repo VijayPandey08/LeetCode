097fe6a5d326a8184890fd7ebd8a9e831a85c934
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-08-15 01:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (50.40%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,59 @@
+class Solution
+{
+    public:
+        int searchInsert(vector<int> &nums, int target)
+        {
+            // int s = 0;
+            // int e = nums.size() - 1;
+            // int ans = 0;
+
+            // while (s <= e)
+            // {
+            //     int mid = e - ((e - s) / 2);
+            //     if (target > nums[mid])
+            //     {   
+            //         s = mid + 1;
+            //         ans=s;
+                
+            //     }
+            //     else if (target < nums[mid])
+            //     {   
+            //         e = mid - 1;
+            //         ans=e;
+                
+            //     }
+            //     else
+            //     {
+            //         // cout<<ans<<endl;
+            //         cout<<s<<" "<<e<<" "<<ans<<endl;
+            //         return ans;
+            //     }
+            // }
+            // cout<<s<<" "<<e<<" "<<ans<<endl;
+            // // return ans+1;
+            
+            // // if(nums[ans]<target){
+            // //     return ans+1;
+            // // }
+            // // else{
+            // //     return ans;
+            // // }
+            // if(ans<0){
+            //     return 0;
+            // }
+            // if(ans>nums.size()-1){
+            //     return ans;
+            // }
+            // else{
+            //     if(nums[ans]>target){
+            //         return ans;
+            //     }
+            //     else{
+            //         return ans +1;
+            //     }
+            // }
+            // // return 0;
+
+            return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        }
+};
