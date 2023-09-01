a5320ee0d029a6e6c9d9fb509c15b571959efb3e
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-09-02 00:59:10 +0530

    Time: 22 ms (77.06%) | Memory: 25.8 MB (9.17%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,20 +3,21 @@ public:
     void rotate(vector<int>& nums, int k) {
         vector<int> temp(nums.size());
         int n=nums.size();
-        // for(int i=0; i<nums.size(); i++){
-        //     temp[(i+k)%nums.size()]=nums[i]; // like a logic u should remember this
-        // }
-        int j=0;
-        for(int i=n-k%n;i<n;i++){
-           temp[j]= nums[i];
-            j++;
+        for(int i=0; i<nums.size(); i++){
+            temp[(i+k)%nums.size()]=nums[i]; // like a logic u should remember this
         }
-        for(int i=0;i<n-k%n;i++){
-            temp[j]=nums[i];
-            j++;
-        }
-       for(int i=0;i<n;i++){
-           nums[i]=temp[i];
-       }
+    //     int j=0;
+    //     for(int i=n-k%n;i<n;i++){
+    //        temp[j]= nums[i];
+    //         j++;
+    //     }
+    //     for(int i=0;i<n-k%n;i++){
+    //         temp[j]=nums[i];
+    //         j++;
+    //     }
+    //    for(int i=0;i<n;i++){
+    //        nums[i]=temp[i];
+    //    }
+    nums=temp;
     }
 };
