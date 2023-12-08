c9a81351484ef76b82209a276a6cbc834b90829d
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-12-08 22:50:07 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (74.34%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,17 +1,22 @@
-class Solution {
-public:
-    bool isHappy(int n) {
-      int count =0;
-      while(n>9){
-          int ans = 0;
-         while(n>0){
-             int dig = n%10;
-             ans += (dig*dig);
-             n = n/10;
-         }
-         n =ans;
-      }  
-      if(n==1|| n==7) return true;
-      return false;
-    }
+class Solution
+{
+    public:
+        bool isHappy(int n)
+        {
+            int count = 10;
+            while (count)
+            {
+            int ans = 0;
+                while (n > 0)
+                {
+                    int digi = n % 10;
+                    ans += (digi *digi);
+                    n = n / 10;
+                }
+                if (ans == 1) return true;
+                n = ans;
+                count--;
+            }
+            return false;
+        }
 };
