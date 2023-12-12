92039996ee641eab1597acd873d506832d2752d6
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-12-12 23:39:12 +0530

    Time: 104 ms (25.06%) | Memory: 93.8 MB (28.69%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy=prices[0];
-        int ans=0;
-        for(int i=0; i<prices.size(); i++){
-            if(buy>prices[i]){
-                buy=prices[i];
-            }
-            else if(buy<prices[i]){
-                ans=max(ans,prices[i]-buy);
-            }
-        }
-        return ans;
+       int buy=prices[0];
+       int sell=0;
+       int profit=INT_MIN;
+       for(int i=0; i<prices.size(); i++){
+           if(buy>prices[i]){
+               buy=prices[i];
+           }
+           else{
+               profit=max(profit,prices[i]-buy);
+           }
+       } 
+       return profit;
     }
 };
