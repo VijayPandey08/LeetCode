7e1b46a826e69a2989de664486d56e81c6f905cc
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-12-14 23:55:17 +0530

    Time: 551 ms (26.07%) | Memory: 179.4 MB (27.59%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,96 +1,79 @@
-class Solution {
-public:
+class Solution
+{
+    public:
 
-void merge(vector<int>& nums, int s, int e) {
+        void merge(vector<int> &nums, int s, int e)
+        {
+            int mid=s+(e-s)/2;
+            int lenLeft = mid - s + 1;
+            int lenRight = e - mid;
+            int *left = new int[lenLeft];
+            int *right = new int[lenRight];
+            int k = s;
+            for (int i = 0; i < lenLeft; i++)
+            {
+                left[i] = nums[k];
+                k++;
+            }
+            k = mid + 1;
+            for (int i = 0; i < lenRight; i++)
+            {
+                right[i] = nums[k];
+                k++;
+            }
 
-	int mid = (s+e)/2;
+            int indLeft = 0;
+            int indRight = 0;
+            int actual = s;
 
-	int lenLeft = mid - s + 1;
-	int lenRight = e - mid;
+            while (indLeft < lenLeft && indRight < lenRight)
+            {
+                if (left[indLeft] < right[indRight])
+                {
+                    nums[actual] = left[indLeft];
+                    actual++;
+                    indLeft++;
+                }
+                else
+                {
+                    nums[actual] = right[indRight];
+                    actual++;
+                    indRight++;
+                }
+            }
 
-	//create left and right array
-	int *left = new int[lenLeft];
-	int *right = new int[lenRight];
+            while (indLeft<lenLeft){
+                nums[actual]=left[indLeft];
+                actual++;
+                indLeft++;
+            }
+                while (indRight<lenRight){
+                nums[actual]=right[indRight];
+                actual++;
+                indRight++;
+            }
 
-	//copy values from original array to left array
-	int k = s;
-	//k -> starting index of left array values in original arrya
-	for(int i=0; i<lenLeft; i++) {
-		left[i] = nums[k];
-		k++;
-	}
+            delete[] left;
+            delete[] right;
+        }
+    void mergeSort(vector<int> &nums, int s, int e)
+    {
+        if (s >= e)
+        {
+            return;
+        }
+        int mid = s + (e - s) / 2;
 
-	//copy values from original array to right array
-	k = mid+1;
-	for(int i=0; i<lenRight; i++) {
-		right[i] = nums[k];
-		k++;
-	}
-
-	//actual merge logic here
-	//left array is already sorted
-	//right array is already sorted
-	int leftIndex = 0;
-	int rightIndex = 0;
-	//yahi pr galti karte h log
-	int mainArrayIndex = s;
-
-	while(leftIndex < lenLeft &&  rightIndex < lenRight) {
-		
-		if(left[leftIndex] < right[rightIndex] ) {
-			nums[mainArrayIndex] =  left[leftIndex];
-			mainArrayIndex++;
-			leftIndex++;
-		}
-		else {
-			nums[mainArrayIndex] =  right[rightIndex];
-			mainArrayIndex++;
-			rightIndex++;
-		}
-	}
-
-
-	//2 more cases
-
-	//1 case -> left array exhaust but right array me element abhi bhi bache hai
-	while(rightIndex < lenRight) {
-		nums[mainArrayIndex] =  right[rightIndex];
-		mainArrayIndex++;
-		rightIndex++;
-	}
-	
-
-	//2nd case -> rigght array exhaust but left array me element abhi bhi bache hai
-	while(leftIndex < lenLeft) {
-		nums[mainArrayIndex] =  left[leftIndex];
-		mainArrayIndex++;
-		leftIndex++;
-	}
-	//1 more step pending
-	delete[] left;
-	delete[] right;
-}
-
-void mergeSort(vector<int>& nums, int s, int e) {
-	//base case
-	if(s >= e) {
-		return;
-	}
-	//break
-	int mid = (s+e)/2;
-	//recusive call for left array
-	mergeSort(nums,s, mid);
-	//recusive call for right array
-	mergeSort(nums, mid+1, e);
-	//merge 2 sorted arrays
-	merge(nums, s, e);
-}
-    vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size();
-        int s=0;
-        int e=nums.size()-1;
-        
-        mergeSort(nums,s,e);
+        mergeSort(nums, s, mid);
+        mergeSort(nums, mid + 1, e);
+        merge(nums, s, e);
+    }
+    vector<int> sortArray(vector<int> &nums)
+    {
+        int n = nums.size();
+        int s = 0;
+        int e = nums.size() - 1;
+        mergeSort(nums, s, e);
         return nums;
     }
 };
