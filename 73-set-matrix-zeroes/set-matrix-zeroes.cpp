ff3c2e4cc6a33a0d23799f723ce3d9c693026ad2
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-12-14 16:01:39 +0530

    Time: 11 ms (60.42%) | Memory: 14.3 MB (8.85%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
 
-// void solve(vector<vector<int>>& matrix, int row, int col){
-//     for(int i=0; i<matrix.size(); i++){
-//             matrix[i][col]=0;
-//     }
-//     for(int i=0; i<matrix[0].size(); i++){
-//             matrix[row][i]=0;
+void solve(vector<vector<int>>& temp, int row, int col){
+    for(int i=0; i<temp.size(); i++){
+            temp[i][col]=0;
+    }
+    for(int i=0; i<temp[0].size(); i++){
+            temp[row][i]=0;
         
-//     }
-// }
+    }
+}
     void setZeroes(vector<vector<int>>& matrix) {
         vector<vector<int>> temp;
         temp=matrix;
@@ -18,21 +18,22 @@ public:
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j]==0){
-                    for(int k=0; k<m; k++){
-                        temp[i][k]=0;
-                    }
+                    solve(temp, i, j);
+                    // for(int k=0; k<m; k++){
+                    //     temp[i][k]=0;
+                    // }
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]==0){
-                    for(int k=0; k<n; k++){
-                        temp[k][j]=0;
-                    }
-                }
-            }
-        }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(matrix[i][j]==0){
+        //             for(int k=0; k<n; k++){
+        //                 temp[k][j]=0;
+        //             }
+        //         }
+        //     }
+        // }
         for(int i=0; i<n; i++){
          for(int j=0; j<m; j++){
             matrix[i][j]=temp[i][j];
