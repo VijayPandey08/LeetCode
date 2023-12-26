4c42b5c64d6e2b1985701dfcceea5a9d2f66e222
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-12-26 21:09:14 +0530

    Time: 2 ms (79.66%) | Memory: 10.2 MB (64.07%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,24 +1,19 @@
-class Solution
-{
-    public:
-        vector<int> finalPrices(vector<int> &prices)
-        {
-            int n = prices.size();
-            vector<int> ans(n);
-            stack<int> st;
-            st.push(0);
-            for (int i = n - 1; i >= 0; i--)
-            {
-                int curr = prices[i];
-               	// answer find krte h ab
-                while (st.top() > curr)
-                {
-                    st.pop();
-                }
-            
-            ans[i] = (curr - st.top());
-            st.push(curr);
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n=prices.size();
+        vector<int> ans(n);
+        stack<int> st;
+        st.push(0);
+        for(int i=n-1; i>=0; i--){
+            int curr = prices[i];
+            // ab har curr ke lie answer find out krenge..
+            while(st.top()>curr){
+                st.pop();
             }
-            return ans;
+            ans[i]=(curr-st.top());
+            st.push(curr);
         }
+        return ans;
+    }
 };
