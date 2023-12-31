531f0305c1a457b959f29a656463a5dee2555aa1
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2023-12-31 08:04:06 +0530

    Time: 6 ms (5.74%) | Memory: 6.9 MB (11.97%) - LeetSync

---
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int maxi=-1;
+        for(int i=0; i<s.length()-1; i++){
+            for(int j=i+1; j<s.length(); j++){
+                if(s[i]==s[j]){
+                    maxi=max(maxi,(j-i-1));
+                }
+            }
+        }
+        return maxi;
+    }
+};
