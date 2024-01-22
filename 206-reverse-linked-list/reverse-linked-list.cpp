c0ad66d6870bc7ce10d5f8809082f9d50161a2b4
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-01-23 04:34:40 +0530

    Time: 3 ms (87.39%) | Memory: 11.6 MB (5.99%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,28 +1,46 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *   int val;
+ *   ListNode * next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
 class Solution
 {
     public:
-        ListNode* reverseList(ListNode *head)
-        {
-           	// iterative approach
-            ListNode *prev = NULL;
-            ListNode *curr = head;
-            while (curr != NULL)
-            {
-                ListNode *nextNode = curr->next;
-                curr->next = prev;
-                prev = curr;
-                curr = nextNode;
+        ListNode* reverse_using_recurssion(ListNode *prev, ListNode *curr) {
+            if(curr==NULL){
+                return prev;
             }
-            return prev;
+            ListNode* nextNode=curr->next;
+            curr->next=prev;
+            return reverse_using_recurssion(curr,nextNode);
+
         }
+    ListNode* reverseList(ListNode *head)
+    {
+
+        ListNode *prev=NULL;
+        ListNode* curr=head;
+        ListNode* ans = reverse_using_recurssion(prev,curr);
+
+       	// iterative approach
+       	// ListNode *prev = NULL;
+       	// ListNode *curr = head;
+       	// while (curr != NULL)
+       	// {
+       	//     ListNode *nextNode = curr->next;
+       	//     curr->next = prev;
+       	//     prev = curr;
+       	//     curr = nextNode;
+       	// }
+       	// return prev;
+
+       	// ab recurssion ki baari
+           return ans;
+
+    }
 };
