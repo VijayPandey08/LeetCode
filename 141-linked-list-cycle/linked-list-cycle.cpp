d7edcb75a611545faaa25a90b6ab721a7d14d344
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-01-24 23:18:41 +0530

    Time: 7 ms (89.09%) | Memory: 10.5 MB (19.36%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,17 +9,34 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-      map<ListNode* , bool> table;
-      ListNode* temp=head;
-      while(temp!=NULL){
-          if(table[temp]==false){
-              table[temp]=true;
-              temp=temp->next;
-          }
-          else{
-              return true;
-          }
-      }  
-      return false;
+   
+   ListNode* slow =head;
+   ListNode* fast=head;
+   while(fast!=NULL){
+       fast=fast->next;
+       if(fast!=NULL){
+           fast=fast->next;
+           slow=slow->next;
+       }
+       if(fast==slow){
+           return true;
+       }
+   }
+   return false;
+   
+   
+    // map vala logic 
+    //   map<ListNode* , bool> table;
+    //   ListNode* temp=head;
+    //   while(temp!=NULL){
+    //       if(table[temp]==false){
+    //           table[temp]=true;
+    //           temp=temp->next;
+    //       }
+    //       else{
+    //           return true;
+    //       }
+    //   }  
+    //   return false;
     }
 };
