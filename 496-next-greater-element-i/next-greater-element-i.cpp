77f7fac1757e8067e419fed757d6dfe6fa652b53
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-02-01 04:02:43 +0530

    Time: 4 ms (67.78%) | Memory: 11.6 MB (16.98%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,37 +1,35 @@
-class Solution {
-public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> s;
-        // s.push(-1);
-        vector<int> temp;
-        map<int,int> m;
-        vector<int> ans;
-        temp=nums2;
-        vector<int> v(nums2.size());
-        reverse(temp.begin(),temp.end());
-        for(int i=0; i<temp.size(); i++){
-            // v.push_back(s.top());
-
-            while(!s.empty() &&  temp[i]>s.top()){
-              s.pop();     
-        }
-                    if(s.empty()){
-                v.push_back(-1);
-                m[temp[i]]=-1;
+class Solution
+{
+    public:
+        vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+        {
+            int n = nums2.size();
+            vector<int> ans(n, -1);
+            stack<int> s;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                while (!s.empty() && s.top() < nums2[i])
+                {
+                    s.pop();
+                }
+                if (!s.empty())
+                {
+                    ans[i] = s.top();
+                }
+                else
+                {
+                    ans[i] = -1;
+                }
+                s.push(nums2[i]);
             }
-        else{
-
-        v.push_back(s.top());
-        m[temp[i]]=s.top();
+            map<int,int> m;
+            for(int i=0; i<nums2.size(); i++){
+                m[nums2[i]]=ans[i];
+            }
+            vector<int> final;
+            for(auto i:nums1){
+                final.push_back(m[i]);
+            }
+            return final;
         }
-        s.push(temp[i]);
-
-
-        }
-        for(auto i:nums1){
-            ans.push_back(m[i]);
-        }
-return ans;
-
-    }
 };
