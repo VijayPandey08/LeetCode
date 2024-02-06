14654acacd3bd227ba82da940e56f23afddbc880
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-02-06 16:32:10 +0530

    Time: 23 ms (22.76%) | Memory: 56.6 MB (15.58%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
@@ -15,28 +15,30 @@ class Solution
 
         string tree2str(TreeNode *root)
         {
-            if (root == NULL)
+
+            if (root == NULL) // base case node mein kuch na bche toh empty string "" return kr do
             {
                 return "";
             }
-            string result = to_string(root->val);
-            string Left = tree2str(root->left);
-            string Right = tree2str(root->right);
+            string result = to_string(root->val); // ek result naam ki string banai aur tree ki root ki value usme daal di
+            string Left = tree2str(root->left); // left se values leke aao
+            string Right = tree2str(root->right); // right se values leke aao aur result m add kr do
 
-            if (root->left == NULL && root->right == NULL)
+            if (root->left == NULL && root->right == NULL) // agar dono taraf null ho jaae toh answer dedo
             {
                 return result;
             }
 
-        if (root->left == NULL) {
-            return result + "()" + "(" + Right + ")";
-        }
+            if (root->left == NULL)  // left side imp h uske lie empty parenthesis add krne pdenge : yeh observation se pta chala
+            {
+                return result + "()" + "(" + Right + ")";
+            }
 
-        if (root->right == NULL) {
-            return result + "(" + Left + ")";
-        }
+            if (root->right == NULL) // agar right side null h toh parenthesis ki koi zrurt nhi
+            {
+                return result + "(" + Left + ")";
+            }
 
-            return result + "(" + Left + ")" + "(" + Right +")";
-    
-}
+            return result + "(" + Left + ")" + "(" + Right + ")"; // ideal case jab left aur right dono side values h ...
+        }
 };
