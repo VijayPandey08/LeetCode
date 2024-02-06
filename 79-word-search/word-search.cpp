7449b90359dc4192f3a41ecdef0d4e4ab37db1a6
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-02-06 22:37:33 +0530

    Time: 302 ms (80.65%) | Memory: 9.6 MB (29.11%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,50 +1,66 @@
 class Solution
 {
     public:
-    int m,n;
-        vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
-        bool find(vector<vector < char>> &board, int i, int j, int idx,  string &word)
-        {
-            
-            if (idx == word.length())
-            {
-                return true;
-            }
-            if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='$'){
-                return false;
-            }
-            if(board[i][j]!=word[idx]){
-                return false;
-            }
-            char temp=board[i][j];
-            board[i][j]='$';
-            for(auto &dir:directions){
-                int new_i = i +dir[0];
-                int new_j = j +dir[1];
+        int m, n;
+    vector<vector < int>> directions
+    {
+        { 1,
+            0 },
+        { -1,
+            0 },
+        { 0,
+            1 },
+        { 0,
+            -1 }
+    };	// dorections vector pehle hi bana dia taake chaaro direction ke lie alag alaga call na krni pde
+    bool find(vector<vector < char>> &board, int i, int j, int idx, string &word)
+    {
 
-                if(find(board, new_i, new_j, idx+1, word)){
-                    return true;
-                }
-            }
-            board[i][j] = temp;
+        if (idx == word.length())
+        {
+            return true;
+        }
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '$')
+        {
+            return false;
+        }
+        if (board[i][j] != word[idx])
+        {
             return false;
         }
 
-    bool exist(vector<vector <char>> &board, string word)
+        char temp = board[i][j];	// ek temp char banaya taki backtrac krte wqt vapas original char daal ske
+        board[i][j] = '$';	// jha traverse kr dia aur char mil gya vha temporarily '$' daal dia taaki vapas na aae usi p
+        for (auto &dir: directions)
+        {
+        	// for loop laga ke chaaaro direction traverse kr li
+            int new_i = i + dir[0];
+            int new_j = j + dir[1];
+
+            if (find(board, new_i, new_j, idx + 1, word))
+            {
+                return true;
+            }
+        }
+        board[i][j] = temp;	// backtrack krte wqt temp ki shi value shi jagah daal di
+        return false;	// agar last tak answer na mile toh false return karwa do
+    }
+
+    bool exist(vector<vector < char>> &board, string word)
     {
-        m=board.size();
-        n=board[0].size();
+        m = board.size();
+        n = board[0].size();
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                if (board[i][j] == word[0] && find(board,i,j,0,word))
+                if (board[i][j] == word[0] && find(board, i, j, 0, word))
                 {
                     return true;
                 }
             }
         }
-        
+
         return false;
     }
 };
