8fa4b49df689349e9ee3dd59f50ba29fa3343715
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-02-06 16:27:43 +0530

    Time: 3 ms (11.43%) | Memory: 7 MB (27.52%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+// reccurssive approach....
     int solve(int n,int k){
-        if(n==1 && k==1){
-            return 0;
+        if(n==1 && k==1){    // base case jab ek hi row hogi aur uska pehla element chahiye hoga : (1st row ka 1st element is 0)
+            return 0; // 1st row ka 1st element is zero
         }
-        int length=pow(2,n-1);
-        int mid = length/2;
-        if(k<=mid){
-            return solve(n-1,k);
+
+        int length=pow(2,n-1); // kisi bhi row mein 2 ki power n-1 elements hi h.
+        int mid = length/2; // har 'nth' row mein mid tak ke elements same h 'n-1' row ke..
+        if(k<=mid){   // yeh cheez observation se hi smjh aaaegi
+            return solve(n-1,k); // har 'nth' row mein mid tak ke elements same h 'n-1' row ke..
         }
         else{
 
-        return !solve(n-1,k-mid);
+        return !solve(n-1,k-mid); // aut mid ke aaage ke elements nikalne k lie k-mid element ka compliment lena pdega
         }
     }
     int kthGrammar(int n, int k) {
