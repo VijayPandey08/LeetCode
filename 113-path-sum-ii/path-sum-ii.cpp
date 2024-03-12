113c17dd930e8d296076dbcbd4325cb616146c3d
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-03-12 21:12:45 +0530

    Time: 19 ms (16.20%) | Memory: 37.8 MB (19.42%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -9,32 +9,47 @@
  *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
+
 class Solution
 {
     public:
-        vector<vector < int>> ans;
-        vector<int> temp;
-    void solve(TreeNode *root, int targetSum, int sum)
+        
+    
+    void solve(TreeNode *root, int targetSum, int sum,vector<vector < int>> &ans,vector<int> temp)
     {
-        if (root == NULL) return;
+        if (root == NULL) return; //dfs
 
-        if(sum+root->val == targetSum && root->left==NULL && root->right==NULL){
-            temp.push_back(root->val);
-            ans.push_back(temp);
-            temp.pop_back();
-            return ;
-        }
+
+        // if(sum+root->val == targetSum && root->left==NULL && root->right==NULL){  // according to question
+        //     temp.push_back(root->val);
+        //     ans.push_back(temp);
+        //     temp.pop_back();
+        //     return ;
+        // }
+
+        // sum+=root->val;             
+        // temp.push_back(root->val); // backtrack (call k pehle)
+        // solve(root->left,targetSum,sum); //dfs
+        // solve(root->right,targetSum,sum); //dfs
+        // temp.pop_back(); // backtrack (call k baad)
 
         sum+=root->val;
         temp.push_back(root->val);
-        solve(root->left,targetSum,sum);
-        solve(root->right,targetSum,sum);
-        temp.pop_back();
-        
+        if(root->left==NULL && root->right==NULL){
+            if(sum==targetSum){
+                ans.push_back(temp);
+                return;
+            }
+        }
+        solve(root->left, targetSum,  sum,ans,temp);
+        solve(root->right, targetSum,  sum,ans,temp);
     }
     vector<vector < int>> pathSum(TreeNode *root, int targetSum)
     {
-        solve(root, targetSum, 0);
+        vector<vector < int>> ans;
+        vector<int> temp;
+        int sum=0;
+        solve(root, targetSum, sum,ans,temp);
         return ans;
     }
 };
