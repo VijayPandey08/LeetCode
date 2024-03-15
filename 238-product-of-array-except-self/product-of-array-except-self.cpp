b049e108dbea07ea5c80eb46519d639ccd37df17
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-03-16 03:22:12 +0530

    Time: 17 ms (47.13%) | Memory: 27.6 MB (14.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,23 +3,26 @@ class Solution
     public:
         vector<int> productExceptSelf(vector<int> &nums)
         {
-            vector<int> ans(nums.size(),0);
+            vector<int> ans(nums.size(),0); // vector intialize kre//
             vector<int> pre(nums.size(),0);
             vector<int> suf(nums.size(),0);
             pre[0] = nums[0];
-            for (int i = 1; i < nums.size(); i++)
+            for (int i = 1; i < nums.size(); i++) // prefix matrix 
             {
                 pre[i] = pre[i - 1] *nums[i];
             }
-            reverse(nums.begin(), nums.end());
+            reverse(nums.begin(), nums.end()); // reverse kia suffix matrix nikalne ke lie,, (asani ho jaati h ese ulta krke multiply krne m)
             suf[0] = nums[0];
             for (int i = 1; i < nums.size(); i++)
             {
                 suf[i] = suf[i - 1] *nums[i];
             }
-            reverse(suf.begin(),suf.end());
-            ans[0] = suf[1];
+            reverse(suf.begin(),suf.end()); // reverse krke correct order nikal lia
+
+            ans[0] = suf[1];                           
             ans[nums.size() - 1] = pre[nums.size() - 2];
+// starting aur ending index constant aane h toh direct dal die
+
             for (int i = 1; i < nums.size()-1; i++) {
                 ans[i]=pre[i-1]*suf[i+1];
             }
