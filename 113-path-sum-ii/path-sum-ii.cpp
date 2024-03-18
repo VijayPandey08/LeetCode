82fe8eb6e004c16e3e37f787e9b69072e72fa450
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-03-19 03:27:40 +0530

    Time: 8 ms (69.53%) | Memory: 18.7 MB (78.63%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -1,55 +1,37 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-
-class Solution
-{
-    public:
-        
-    
-    void solve(TreeNode *root, int targetSum, int sum,vector<vector < int>> &ans,vector<int> temp)
-    {
-        if (root == NULL) return; //dfs
-
-
-        // if(sum+root->val == targetSum && root->left==NULL && root->right==NULL){  // according to question
-        //     temp.push_back(root->val);
-        //     ans.push_back(temp);
-        //     temp.pop_back();
-        //     return ;
-        // }
-
-        // sum+=root->val;             
-        // temp.push_back(root->val); // backtrack (call k pehle)
-        // solve(root->left,targetSum,sum); //dfs
-        // solve(root->right,targetSum,sum); //dfs
-        // temp.pop_back(); // backtrack (call k baad)
-
-        sum+=root->val;
-        temp.push_back(root->val);
-        if(root->left==NULL && root->right==NULL){
-            if(sum==targetSum){
-                ans.push_back(temp);
-                return;
-            }
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+    void solve(TreeNode* root, int targetSum, int sum){
+            if(root==NULL){
+            return ;
         }
-        solve(root->left, targetSum,  sum,ans,temp);
-        solve(root->right, targetSum,  sum,ans,temp);
+  temp.push_back(root->val);
+  sum+=root->val;
+        if(root->left==NULL && root->right==NULL && sum==targetSum){
+            ans.push_back(temp);
+        }
+
+
+        solve(root->left,targetSum,sum);
+        solve(root->right,targetSum,sum);
+        temp.pop_back();
     }
-    vector<vector < int>> pathSum(TreeNode *root, int targetSum)
-    {
-        vector<vector < int>> ans;
-        vector<int> temp;
-        int sum=0;
-        solve(root, targetSum, sum,ans,temp);
-        return ans;
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+          int sum=0;
+          solve(root,targetSum,sum);
+          return ans;
+        
     }
 };
