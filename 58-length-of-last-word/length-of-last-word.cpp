fe0972e8f7c656ae0ace3eb969d00e26d0c05589
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-04-01 23:17:29 +0530

    Time: 4 ms (47.87%) | Memory: 7.6 MB (57.50%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,30 +1,19 @@
-class Solution
-{
-    public:
-        int lengthOfLastWord(string s)
-        {
-            int ans = 0;
-            vector<char> v;
-
-            for (int i = s.length() - 1; i >= 0; i--)
-            { 
-                // agar last mein space ho toh uss case mein aage bdh jaao continue krke
-                if (s[i] == ' ' && ans == 0)
-                {
-                    continue;
-                }
-                // agar space nhi h, toh ans++ krdo
-                if (s[i] != ' ')
-                {
-                    ans++;
-                }
-                //jesi pehla space mile  ans>0(mtlb last word ke bad) break kr do
-               if (s[i] == ' ' && ans > 0)
-                {
-                    break;
-                }
-            }
-
-            return ans;
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int k = s.length();
+        int ans=0;
+        while(k--){
+              if(s[k]==' ' && ans==0){
+                continue;
+              }
+              else if(s[k]!= ' '){
+                ans++;
+              }
+              else if(s[k]==' ' && ans>0){
+                return ans;
+              }
         }
+        return ans;
+    }
 };
