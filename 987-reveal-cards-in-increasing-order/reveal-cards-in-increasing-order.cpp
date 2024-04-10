a9db971b8fd3f25bd8d29a215dd8babb12a6cf22
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-04-11 03:14:56 +0530

    Time: 3 ms (67.50%) | Memory: 10.9 MB (67.60%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
-        int i=0;
-        int j=0;
-        vector<int> temp(deck.size(),0);
-        sort(deck.begin(),deck.end());
-        bool flag = false;
-        while(i<deck.size()){
-            if(temp[j]==0){
-                if(flag==false){
-                    temp[j]=deck[i];
-                    i++;
-                }
-                flag=!flag;
-            }
-            j = (j+1)%deck.size();
-        }
-        return temp;
+        // int i=0; // TO ITERATE DECK
+        // int j=0; // TO ITERATE RESULT
+        // vector<int> temp(deck.size(),0); // ISME ANSWER STORE KRUNGA
+        // sort(deck.begin(),deck.end());  // DECK KO SORT KRO..
+        // bool flag = false; // EK FLAG BANA DIA KI Jth index pe answer bharunga nahi
+        // while(i<deck.size()){ // deck ka hr ek element jab daal denge tab answer ban jaaega
+        //     if(temp[j]==0){ // temp vector mein pehle se sab zero kar dia tha.. ab jab zero hoga tab hi dalenge
+        //         if(flag==false){ // plus plag bhi false hona chahiye tab hi dalenge
+        //             temp[j]=deck[i];
+        //             i++; // main deck mein aage badh jaao jab daldo ek cheez
+        //         }
+        //         flag=!flag; // frr flag ko vapas se set kr do opposite, taki agli baar na dalo ya alternate krke hi dalo
+        //     }
+        //     j = (j+1)%deck.size(); // Jth index ko ese bdhana h ki ek loop mein rkhna h
+        // }
+        // return temp; 
+// approach 2
+
+queue<int> dq;
+vector<int> ans(deck.size());
+
+sort(deck.begin(),deck.end());
+
+for(int i=0; i<deck.size(); i++){
+    dq.push(i);
+}
+
+for(int i=0; i<deck.size(); i++){
+    int val = dq.front();
+    dq.pop();
+
+    ans[val]= deck[i];
+
+    if(!dq.empty()){
+    dq.push(dq.front());
+    dq.pop();
+    }
+
+}
+return ans;
+
     }
 };
