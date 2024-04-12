a531ab31cb5cf819a51f00545c70fc1853412f4e
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-04-13 02:50:38 +0530

    Time: 357 ms (10.72%) | Memory: 16.2 MB (62.18%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,32 +1,50 @@
 class Solution {
 public:
 
-bool solve(vector<int>& nums,int index, int target, vector<vector<int>> &dp){
-    if(index>=nums.size()){
-        if(target==0) return true;
-        return false;
-    } 
-    if(target<0) return false;
-    if(target==0) return true;
+// bool solve(vector<int>& nums,int index, int target, vector<vector<int>> &dp){
+// // recursion
 
-    if(dp[index][target]!=-1){
-        return dp[index][target];
-    }
+//     if(index>=nums.size()){
+//         if(target==0) return true;
+//         return false;
+//     } 
+//     if(target<0) return false;
+//     if(target==0) return true;
 
-    bool include = solve(nums,index+1,target-nums[index],dp);
-    bool exclude = solve(nums,index+1,target,dp);
+//     if(dp[index][target]!=-1){
+//         return dp[index][target];
+//     }
 
-    return dp[index][target]=(include | exclude);
+//     bool include = solve(nums,index+1,target-nums[index],dp);
+//     bool exclude = solve(nums,index+1,target,dp);
 
-}
+//     return dp[index][target]=(include | exclude);
+
+// }
     bool canPartition(vector<int>& nums) {
        int n = nums.size();
        int sum = accumulate(nums.begin(),nums.end(),0ll);
        if(sum&1) return false;
        int target = sum /2;
-        vector<vector<int>> dp(nums.size()+1,vector<int>(target+1,-1));
-       cout<<target<<endl;
-       return solve(nums,0,target,dp);
+        vector<vector<bool>> dp(nums.size()+1,vector<bool>(target+1,false));
+    //    cout<<target<<endl;
+    //    return solve(nums,0,target,dp);
+    for(int i=0; i<dp.size(); i++){
+        dp[i][0]=true;
+    }
+    for(int i=1; i<dp.size(); i++){
+        for(int j=1; j<target+1; j++){
+               if(j>=nums[i-1]){
+                dp[i][j]=dp[i-1][j] | dp[i-1][j-nums[i-1]];
+               }
+               else{
+                 dp[i][j] = dp[i-1][j];
+               }
+        }
+    }
+    return dp[nums.size()][target];
+
+
 
 
     }
