b6cbc5fc8dbd99f37de88ac88c14d1700000e53d
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-04-19 04:32:50 +0530

    Time: 91 ms (17.98%) | Memory: 103.6 MB (18.79%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
+    vector<vector<int>> direction = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     int islandPerimeter(vector<vector<int>>& grid) {
-        int islands = 0;
-        int neighbors = 0;
-
-        for (int i = 0; i < grid.size(); ++i) {
-            for (int j = 0; j < grid[0].size(); ++j) {
-                if (grid[i][j] == 1) {
-                    islands++;
-                    if (i - 1 >= 0 && grid[i - 1][j] == 1) {
-                        neighbors++;
+        // dfs
+        int ans = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0)
+                    continue;
+                else {
+                    if (i +1 >= n || grid[i + 1][j] == 0) {
+                        ans++;
                     }
-                    if (j - 1 >= 0 && grid[i][j - 1] == 1) {
-                        neighbors++;
+                    if (i - 1 < 0 || grid[i - 1][j] == 0) {
+                        ans++;
+                    }
+                    if (j + 1 >= m || grid[i][j+1] == 0) {
+                        ans++;
+                    }
+                    if (j - 1 < 0 || grid[i][j - 1] == 0) {
+                        ans++;
                     }
                 }
             }
         }
-
-        return islands * 4 - neighbors * 2;
+        return ans;
     }
 };
