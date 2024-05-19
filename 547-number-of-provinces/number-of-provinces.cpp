db8ef535e058004666d50adc1fbdfa4b8d329c75
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-05-19 16:02:02 +0530

    Time: 30 ms (10.24%) | Memory: 20.8 MB (10.02%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj , int i, vector<bool> &visited) {
-        if(visited[i]){
-            return;
-        }
-        visited[i]=true;
-        for(auto v: adj[i]){
+
+void dfs(unordered_map<int,vector<int>> &adj, int i, vector<bool> &visited){
+    visited[i]=true;
+    for(auto v:adj[i]){
+        if(!visited[v]){
             dfs(adj,v,visited);
         }
     }
-    int findCircleNum(vector<vector<int>>& is) {
-        int m = is.size();
-        int n = is[0].size();
-        int count = 0;
-        vector<vector<int>> adj(m);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (is[i][j] == 1) {
+}
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        int m=isConnected[0].size();
+        // let us first make a graph 
+        unordered_map<int,vector<int>> adj;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(isConnected[i][j]==1){
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
-      vector<bool> visited(m,false);
-      for(int i=0; i<m; i++){
-        if(visited[i]==false){
-            dfs(adj, i , visited);
-            count++;
+        int count=0;  //for storing ans
+        vector<bool> visited(n,false);
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                dfs(adj,i,visited);
+                count++;
+            }
         }
-      }
-
-        // for(auto i:is){
-        //     for(auto j:i){
-        //         cout<<j<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         return count;
     }
 };
