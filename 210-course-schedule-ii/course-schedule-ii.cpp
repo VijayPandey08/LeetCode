dda78213055b285ac90f4b26c2660bc3ebace74c
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-05-19 19:14:56 +0530

    Time: 26 ms (7.86%) | Memory: 20 MB (7.87%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,42 +1,54 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
-        unordered_map<int,vector<int>> adj;
-        int count=0;
-        vector<int> result;
-        vector<int> indegree(numCourses,0);
-        queue<int> q;
-        vector<int> v;
+bool has_cycle;
+void dfs(unordered_map<int,vector<int>> &adj, int i,vector<bool> &visited,vector<bool> &in_recursion, stack<int> &s ){
+    visited[i]=true;
+    in_recursion[i]=true;
+    for(auto v:adj[i]){
+        if(in_recursion[v]==true){
+            has_cycle=true;
+            return;
+        }
+        if(!visited[v]){
+            dfs(adj,v,visited,in_recursion,s);
+        }
+    }
+    in_recursion[i]=false;
+    s.push(i);
 
-        // making required graph
-        for(auto i:pre){
+}
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+        // chalo bhaisahab isko solve krna h dfs se, 
+        // pehle check krenge cycle toh nhi h, agar cycle h toh baat khtm khali vector return krwaenge
+        // vrna topological order de denge
+
+        unordered_map<int,vector<int>> adj;
+        vector<bool> visited(numCourses,false);
+        vector<bool> in_recursion(numCourses,false);
+        stack<int> s;
+
+
+        // making graph
+        for(auto i: pre){
             int a = i[0];
             int b = i[1];
             // b------->a
             adj[b].push_back(a);
-            indegree[a]++;
         }
 
-        for(int i=0; i<indegree.size(); i++){
-            if(indegree[i]==0){
-               q.push(i);
+        for(int i=0; i<numCourses; i++){
+            if(!visited[i]){
+                dfs(adj,i,visited,in_recursion,s);
             }
         }
-
-        while(!q.empty()){
-            int u = q.front();
-            count++;
-            result.push_back(u);
-            q.pop();
-            for(auto v:adj[u]){
-                indegree[v]--;
-                if(indegree[v]==0){
-                    q.push(v);
-                }
-            }
+        if(has_cycle){
+            return {};
         }
-        if(count==numCourses) return result;
-        return v;
-
+vector<int> result;
+        while(!s.empty()){
+              result.push_back(s.top());
+              s.pop();
+        }
+        return result;
     }
 };
