bb51e341806b3d0355e95937aa1b2f7ea0b7bbd7
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-05-19 18:47:33 +0530

    Time: 26 ms (10.08%) | Memory: 19.7 MB (11.08%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& pre) {
-        unordered_map<int, vector<int>> adj;
-        vector<int> indegree(numCourses, 0);
-        queue<int> q;
-        int count=0;
-
-        // making graph
-        for (auto i : pre) {
-            int a = i[0];
-            int b = i[1];
-            //   b------->a
-            adj[b].push_back(a);
-            // indegree[a]++;
-        }
-
-        for (int u = 0; u < numCourses; u++) {
-            for (auto v : adj[u]) {
-                indegree[v]++;
-            }
-        }
-
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        while (!q.empty()) {
-            int node = q.front();
-            count++;
-            q.pop();
-            for (auto v : adj[node]) {
-                indegree[v]--;
-                if (indegree[v] == 0) {
-                    q.push(v);
-                }
-            }
-        }
-
-        if (count == numCourses)
+bool dfs(unordered_map<int,vector<int>> &adj, int i, vector<bool> &visited, vector<bool> &in_recursion){
+    visited[i]=true;
+    in_recursion[i]=true;
+    for(auto v:adj[i]){
+        if(!visited[v] && dfs(adj,v,visited,in_recursion)){
             return true;
-        return false;
+        }
+        else if(in_recursion[v]==true) return true;
+    }
+    in_recursion[i]=false;
+    return false;
+}
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        //------------DFS--------------
+        unordered_map<int,vector<int>> adj;
+        vector<bool> visited(numCourses,false);
+        vector<bool> in_recursion(numCourses,false);
+
+        // making graph....
+        for(auto i:pre){
+            int a =i[0];
+            int b =i[1];
+            // b-------->a
+            adj[b].push_back(a);
+        }
+
+        for(int i=0; i<numCourses; i++){
+            if(!visited[i] &&  dfs(adj,i,visited,in_recursion)){
+                return false;
+            }
+        }
+        return true;
+
     }
 };
