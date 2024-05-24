8028237876ea7f8a2b0ac3d534a33e2215bd83a2
Vijay Pandey <vijaypandey0825@gmail.com>
2024-05-24 14:26:27 +0530

    Time: 278 ms (5.79%) | Memory: 82.6 MB (6.40%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,46 +1,51 @@
 class Solution {
 public:
-    int m, n;
-    vector<vector<int>> directions{{0, 1}, {1, 0},  {-1, 0}, {0, -1},
-                                   {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-    bool isSafe(int x_, int y_) {
-        return (x_ >= 0 && x_ < n && y_ >= 0 && y_ < m);
-    }
+int n,m;
+typedef pair<int,pair<int,int>> P;
+vector<vector<int>> directions{{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+bool isSafe(int x_, int y_){
+    return (x_>=0 && y_>=0 && x_<n && y_<m); 
+}
+// yeh isliye banaya q ki, hume priority_queue mein {distance, {cell ke coordinates store krne h}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        // yeh h second approach the great dijkstras..
+
         n = grid.size();
         m = grid[0].size();
-        if (m == 0 || n == 0 || grid[0][0] != 0)
-            return -1;
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        grid[0][0] = 1;
+        // baar baar n,m ka istemal pdega, bs n aur m mein confuse nahi hone ka
 
-        int level = 1;
+        if(n==0 || m==0 || grid[0][0]!=0) return -1; // basic check for formality
+        // hume banana h priority_queue aur reult vector
+        vector<vector<int>> result(n,vector<int>(m,INT_MAX));
+        result[0][0]=0;
+        priority_queue<P, vector<P>, greater<P>> pq;
+        pq.push({0,{0,0}});
 
-        while (!q.empty()) {
-            int N = q.size();
-            while (N--) {
-                auto curr = q.front();
-                q.pop();
-                int x = curr.first;
-                int y = curr.second;
+        while(!pq.empty()){
+            int d = pq.top().first;
+            pair<int,int> node = pq.top().second;
+            int node_x = node.first;
+            int node_y = node.second;
+            pq.pop();
+            
+            for(auto dir:directions){
+               int x_ = node_x + dir[0];
+               int y_ = node_y + dir[1];
+               int dis = 1;
 
-                if (x == n - 1 && y == m - 1)
-                    return level;
-
-                for (auto dir : directions) {
-                    int x_ = x + dir[0];
-                    int y_ = y + dir[1];
-
-                    if (isSafe(x_, y_) && grid[x_][y_] == 0) {
-                        q.push({x_, y_});
-                        grid[x_][y_] = 1;
-                    }
-                }
+               if(isSafe(x_,y_) && grid[x_][y_]==0 && d + dis < result[x_][y_] ){
+                pq.push({d+dis,{x_,y_}});
+                result[x_][y_] = d +dis;
+                grid[x_][y_] = 1;
+               }
             }
-            level++;
-        }
+            
 
-        return -1;
+        }
+        if(result[n-1][m-1]==INT_MAX) return -1;
+        return result[n-1][m-1] +1;
+        
+
+
     }
 };
