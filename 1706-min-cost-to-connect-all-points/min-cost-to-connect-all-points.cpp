d6b6ccda6d24fa7659b6eee15c8e34a83f8db34a
Vijay Pandey <vijaypandey0825@gmail.com>
2024-05-27 15:45:34 +0530

    Time: 1025 ms (15.35%) | Memory: 250 MB (11.36%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,62 +1,75 @@
 class Solution {
 public:
-// / this is calssical implementation of prims algorithm...... /////
+    vector<int> parent;
+    vector<int> rank;
 
+    int find(int i) {
+        if (parent[i] == i)
+            return i;
 
-    int prims_algo(vector<vector<pair<int,int>>> &adj, int V) {
-        // Priority queue to store the minimum weight edge at top
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        // Start with node 0 (with 0 distance)
-        pq.push({0, 0});
-        // To track nodes included in MST
-        vector<bool> inMst(V, false);
-        int sum = 0;
-
-        while(!pq.empty()) {
-            // Get the node with the smallest distance
-            int dis = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-
-            // If the node is already included in MST, continue
-            if(inMst[node]) continue;
-            // Include the node in MST and add its distance to the sum
-            inMst[node] = true;
-            sum += dis;
-
-            // For all adjacent nodes, push them to the priority queue if not already in MST
-            for(auto v : adj[node]) {
-                int adj_node = v.first;
-                int adj_dis = v.second;
-                if(!inMst[adj_node]) {
-                    pq.push({adj_dis, adj_node});
-                }
-            }
-        }
-        return sum;
+        return parent[i] = find(parent[i]);
     }
 
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int V = points.size();
-        vector<vector<pair<int,int>>> adj(V);
+    void Union(int u, int v) {
+        int parent_u = find(u);
+        int parent_v = find(v);
+        if (rank[parent_u] > rank[parent_v]) {
+            parent[parent_v] = parent_u;
+        } else if (rank[parent_v] > rank[parent_u]) {
+            parent[parent_u] = parent_v;
+        } else {
+            parent[parent_u] = parent_v;
+            rank[parent_v]++;
+        }
+    }
+    int kruskal(vector<vector<int>>& vec, int V) {
+        parent.resize(V, 0);
+        rank.resize(V, 0);
 
-        // Construct the adjacency list
-        for(int i = 0; i < points.size(); i++) {
-            for(int j = i + 1; j < points.size(); j++) {
+        for (int i = 0; i < V; i++) {
+            parent[i] = i;
+        }
+
+        int sum = 0;
+
+        
+            for (auto vp : vec) {
+                int u = vp[0];
+                int v = vp[1];
+                int w = vp[2];
+
+                int parent_u = find(u);
+                int parent_v = find(v);
+
+                if (parent_u != parent_v) {
+                    Union(u, v);
+                    sum += w;
+                }
+            }
+        
+        return sum;
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        vector<vector<int>> vec;
+        int V = points.size();
+
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
                 int x1 = points[i][0];
                 int y1 = points[i][1];
+
                 int x2 = points[j][0];
                 int y2 = points[j][1];
 
-                int d = abs(x1 - x2) + abs(y1 - y2);
+                int w = abs(x1 - x2) + abs(y1 - y2);
 
-                adj[i].push_back({j, d});
-                adj[j].push_back({i, d});
+                vec.push_back({i, j, w});
             }
         }
-
-        // Run Prim's algorithm to get the minimum spanning tree cost
-        return prims_algo(adj, V);
-
+        auto comprator = [](vector<int>& v1, vector<int>& v2) {
+            return v1[2] < v2[2];
+        };
+        sort(vec.begin(), vec.end(), comprator);
+        return kruskal(vec, V);
     }
 };
