e0c97b76cb79c6ab6d8d4ff860213bcfaad98d23
Vijay Pandey <vijaypandey0825@gmail.com>
2024-05-30 16:27:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (49.44%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -2,25 +2,52 @@ class Solution {
 public:
     int countTriplets(vector<int>& arr) {
         // ---- brute force approach //-----
+        // int n = arr.size();
+        // int pairs = 0;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         int a = 0;
+        //         for (int k = i; k < j; k++) {
+        //             a ^= arr[k];
+        //         }
+
+        //         int b = 0;
+
+        //         for (int k = j; k < n; k++) {
+        //             b ^= arr[k];
+        //             if (a == b) {
+        //                 pairs++;
+        //             }
+        //         }
+        //     }
+        // }
+        // return pairs;
+
+        //--- ab bdiya optimised approach dekhte h
+        //--- isme use krenge XOR ki bdiya property,
+        //--- key points used:  prefix xor,  similar to equal subarray sum
+
         int n = arr.size();
-        int pairs = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int a = 0;
-                for (int k = i; k < j; k++) {
-                    a ^= arr[k];
-                }
+        vector<int> pre(n + 1, 0); // prefix XOR array
+        int count = 0;
 
-                int b = 0;
+        // Compute the prefix XOR array
+        for (int i = 0; i < n; ++i) {
+            pre[i + 1] = pre[i] ^ arr[i];
+        }
 
-                for (int k = j; k < n; k++) {
-                    b ^= arr[k];
-                    if (a == b) {
-                        pairs++;
-                    }
+        // Iterate over all pairs (i, k)
+        for (int i = 0; i < n; ++i) {
+            for (int k = i + 1; k <= n; ++k) {
+                if (pre[i] == pre[k]) {
+                    count += k - i - 1;
                 }
             }
         }
-        return pairs;
+        
+        return count;
+
+
+    
     }
 };
