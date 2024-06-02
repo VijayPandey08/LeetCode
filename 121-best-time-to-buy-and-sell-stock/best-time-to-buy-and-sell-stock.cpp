88562ab5ebb703a40f02e1d03352b5c57637ca28
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-02 16:23:16 +0530

    Time: 70 ms (95.72%) | Memory: 95.9 MB (32.39%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0;
-        int buy = prices[0];
+        // brute forces toh yehi hota ki tum do looop lagate aur dekh lete kha
+        // pr difference zada h, pr esa kr nhi skte q ki, constraint 10^8 se
+        // uppar nikal jaaenge do for loop se
 
-        for(int i=1; i<prices.size(); i++){
-            if(prices[i]<buy){
-                buy = prices[i];
-            }
-            else if(prices[i]>buy){
-                ans = max(ans,abs(prices[i]-buy));
+
+        // yeh bdiya jugadu tareeka h, q ki ek hi iteration mein kaam poora krna h
+        int ans = 0;
+        int buy = prices[0]; // maanlo ki pehla hi khareed lia h
+
+        // ab iske next element se iteration se shuru kro
+        // agar kam ka huea toh seedha use khareed lena
+
+        // agar zada ka bik raha ho toh profit store kr lena, aur aage bdhna 
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];    
+            } else if (prices[i] > buy) {
+                ans = max(ans, abs(prices[i] - buy));
             }
         }
         return ans;
