cfafd8250d2806e34b4e737292982cfdb4e33c57
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-03 15:28:31 +0530

    Time: 4 ms (38.27%) | Memory: 8.8 MB (21.83%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size()-1;
-        vector<vector<int>> result(n+1,vector<int>(n+1));
-        for(int i =0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                 result[j][n-i]=matrix[i][j];
+        // in this we have to take extra space...
+        // but we need to find a better approach in which no space is required
+        // int n=matrix.size()-1;
+        // vector<vector<int>> result(n+1,vector<int>(n+1));
+        // for(int i =0; i<matrix.size(); i++){
+        //     for(int j=0; j<matrix[0].size(); j++){
+        //          result[j][n-i]=matrix[i][j];
+        //     }
+        // }
+        // matrix=result;
+        // return ;
+
+        /// -------------- approach 2 --------------////
+        // it is basically a observation based approach..
+        // STEP 1: TAKE TRANSPOSE
+        // STEP 2: REVERSE THE MATRIX
+        // done...!
+
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                // for transpose we simply use swap function
+                // transpose karna bhi apne aap mein ek kala hein..
+                // prime diagonal chedhne ki zrurat nhi, j =0 se j< i tak hi
+                // jaaaega----> very imp
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        matrix=result;
-        return ;
+        for (int i = 0; i < matrix.size(); i++) {
+            // reverse krna h row ke hisab se...toh dekh lo, row tak iterate kia
+            // haur matrix[i].begin(),matrix[i].end() maar dia
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+        return;
     }
 };
