e7631f0d00dd83cc318a30f0a11cf5880f3a8cac
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-03 16:17:47 +0530

    Time: 19 ms (59.76%) | Memory: 11.9 MB (83.42%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -3,18 +3,18 @@ public:
     int appendCharacters(string s, string t) {
         int i=0;
         int j=0;
-        int n = t.size();
+        // int n = t.size();
         while(i<s.size() && j<t.size()){
             if(t[j]==s[i]){
                 j++;
                 i++;
-                n--;
-            cout<<n<<" ";
+                // n--;
+            // cout<<n<<" ";
             }
             else if(t[j]!=s[i]){
                 i++;
             }
         }
-        return n;
+        return t.size()-j;
     }
 };
