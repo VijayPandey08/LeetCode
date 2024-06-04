b7982acb29aea05722831d20f8ab1b398f9d6cac
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-04 16:57:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (62.59%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -7,12 +7,12 @@ public:
         }
         int count=0;
         for(auto i: m){
-            if(i.second>1  ){
+            if(i.second>1){
                  count+=i.second/2;
             }
         }
         cout<<count;
-        if(2*count == s.length()) return 2*count;
+        // if(2*count == s.length()) return 2*count;
         if(2*count<s.length()) return 2*count + 1;
         return 2*count;
         // return count;
