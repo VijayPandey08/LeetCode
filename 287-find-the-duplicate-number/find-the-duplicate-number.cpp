6df519c7d3d119b963d02366fa6e20bc7d9191f0
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-04 16:54:53 +0530

    Time: 78 ms (61.62%) | Memory: 63.7 MB (52.38%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,16 +1,42 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        map<int,int> m;
-        for(int i=0; i<nums.size(); i++){
-            m[nums[i]]++;
+        // we will use here and tortoise algorithm
+        // observation : duplicate numbers
+        
+        // here and tortoise algorithm simply helps in detection a cycle
+        // and determining the start of the circle
+
+        // first time fast == slow : cycle is present
+        // then from here move slow to zero, then move fast and slow by one space each and when again they will meet i.e fast == slow then this will be the required ans
+        // return fast/slow
+
+// STEP 1 : MOVING SLOW one space... and FAST two space
+        int slow = nums[0];
+        int fast = nums[0];
+
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+
+
+//STEP-2 : DETECTING CYCLE
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
 
-        for(auto i:m){
-            if(i.second>1){
-                return i.first;
-            }
+// STEP 3 : After cycle is confirmed, bring slow back to start
+        slow = nums[0];
+
+
+// STEP 4:  move both slow and fast one one space to find starting point of cylcle
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return 0;
+
+        return fast;
+        // return slow;
+        
     }
 };
