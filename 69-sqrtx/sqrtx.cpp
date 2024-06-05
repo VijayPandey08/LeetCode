83e14c8378ff697f33d2468ae68526eb5b46089c
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-05 18:44:08 +0530

    Time: 2 ms (51.25%) | Memory: 7.4 MB (80.77%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -5,15 +5,15 @@ public:
         int e=x;
         int ans=0;
     
-
+//---------very standard but imp thing to note my friend-----------
         while(s<=e){
         long long mid = e-((e-s)/2);
             if((mid*mid)==x){
-                return mid;
+                return mid;   // agar perfect square mile toh vhi return kr jaao
             }
             else if((mid*mid)<x){
                 s=mid+1;
-                ans = mid;
+                ans = mid;  /// agar perfect square na ho toh uske neeche vala rkh lo apne pas
 
             }
             else {
