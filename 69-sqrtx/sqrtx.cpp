5c8bddf300708e9927eed3880d5204375d0b5312
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-06 15:27:05 +0530

    Time: 5 ms (35.47%) | Memory: 7.2 MB (97.03%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int s=0;
-        int e=x;
-        int ans=0;
-    
-//---------very standard but imp thing to note my friend-----------
-        while(s<=e){
-        long long mid = e-((e-s)/2);
-            if((mid*mid)==x){
-                return mid;   // agar perfect square mile toh vhi return kr jaao
-            }
-            else if((mid*mid)<x){
-                s=mid+1;
-                ans = mid;  /// agar perfect square na ho toh uske neeche vala rkh lo apne pas
+        if (x == 0) return 0; 
+        int s =1;
+        int e =x;
 
-            }
-            else {
-                e=mid-1;
-            }
+        int ans=0;
+       
+        while(s<=e){
+           long long mid = s + (e-s)/2;
+
+           if((mid*mid)==x){
+                return mid;
+           }
+           else if((mid*mid)<x){
+            s=mid+1;
+            ans=mid;
+           }
+           else{
+            e=mid-1;
+           }
         }
         return ans;
     }
