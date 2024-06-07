b9a387a6f37627f099dc967ce60907695fb9c6c6
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-07 15:06:04 +0530

    Time: 123 ms (12.93%) | Memory: 12.7 MB (74.21%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,57 +1,17 @@
-class Solution
-{
-    public:
-        vector<int> twoSum(vector<int> &nums, int target)
-        {
-            vector<int> temp;
-            vector<int> ans;
-            temp = nums;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
 
-            sort(nums.begin(), nums.end());
-
-            int s = 0;
-            int e = nums.size() - 1;
-
-            int check1 = 0;
-            int check2 = 0;
-
-            while (s < e)
-            {
-                int mid = nums[s] + nums[e];
-                if (mid == target)
-                {
-                    check1 = nums[s];
-                    check2 = nums[e];
-                    break;
-                
-                }
-                else if (mid > target)
-                {
-                    e--;
-                }
-                else if (mid < target)
-                {
-                    s++;
-                }
-            }
-
-            for (int i = 0; i < temp.size(); i++)
-            {
-                if (temp[i] == check1)
-                {
-                    ans.push_back(i);
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i]+nums[j]==target){
+                    result.push_back(i);
+                    result.push_back(j);
                     break;
                 }
             }
-            for (int i = temp.size() - 1; i >= 0; i--)
-            {
-                if (temp[i] == check2)
-                {
-                    ans.push_back(i);
-                    break;
-                }
-            }
-
-            return ans;
         }
+        return result;
+    }
 };
