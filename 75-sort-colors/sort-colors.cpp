9c09f8be801d1d2b5bbeba8203da255eafc10f18
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-12 13:33:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (34.14%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // hi -fi question h, angrejo ki bhansha mein, dutch national flag algorithm mein based
-        int s=0;  // yeh starting pointer h, ise bs zero se matlab h
-        int m=0; // yeh h asli khiladi, pura iterate krega jo shi dhng se
-        int e=nums.size()-1; // yeh high pointer h ise bs 2 se mtlb h
+        int s=0;
+        int i=0;
+        int e=nums.size()-1;
 
-        while(m<=e){
-            if(nums[m]==0){
-                swap(nums[s],nums[m]);
-                    m++;
-                    s++;
+        while(i<=e){
+            if(nums[i]==0){
+                swap(nums[i],nums[s]);
+                s++;
+                i++;
+                
             }
-            else if(nums[m]==1){
-                m++;
+            else if(nums[i]==2){
+                swap(nums[i],nums[e]);
+                e--;
+                
             }
             else{
-                swap(nums[m],nums[e]);
-                e--;
+                i++;
             }
+
         }
         return ;
     }
