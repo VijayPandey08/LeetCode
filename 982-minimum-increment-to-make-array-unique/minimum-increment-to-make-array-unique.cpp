2276e9d5554584ae4934247835508328a999737e
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-14 17:53:42 +0530

    Time: 114 ms (76.77%) | Memory: 69.2 MB (24.61%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        map<int, int> m;
-        // for(auto i:nums){
-        //     m[i]++;
-        // }
-
         sort(nums.begin(), nums.end());
 
         int ans = 0;
@@ -13,19 +8,16 @@ public:
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i] > nums[i - 1]) {
                 continue;
-            } else if (nums[i] == nums[i - 1]) {
+            } 
+            else if (nums[i] == nums[i - 1]) {
                 ans++;
                 nums[i] = nums[i] + 1;
-                // continue;
-            } else if (nums[i] < nums[i - 1]) {
+            } 
+            else if (nums[i] < nums[i - 1]) {
                 ans += abs(nums[i] - nums[i - 1]) + 1;
                 nums[i] = nums[i] + abs(nums[i] - nums[i - 1]) + 1;
             }
         }
-
-        // for(auto i:nums){
-        //     cout<<i<<" ";
-        // }
 
         return ans;
     }
