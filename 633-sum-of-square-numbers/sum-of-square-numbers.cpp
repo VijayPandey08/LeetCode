e591803fb901df9ca5c8ab44ada50c2b995da5c2
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-17 18:31:14 +0530

    Time: 3 ms (68.00%) | Memory: 7.1 MB (18.45%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-       long long a =0;
-       long long b =sqrt(c);
+        // good implementation of two pointer
+        long long a = 0;
+        long long b = sqrt(c);
+        // if we take the most extreme case then c would be a perfect square and
+        // then a would be 0 in that case and b would be its sqaure root so in
+        // conclusion we can say that a and b should be between 0 and the sqare
+        // root of c
 
-       while(a<=b){
-        long long k = (a*a) + (b*b);
-        if(k<c){
-            a++;
+        // normal binary search type approach
+        while (a <= b) {
+            long long k = (a * a) + (b * b);
+            if (k < c) {
+                a++;
+            } else if (k > c) {
+                b--;
+            } else {
+                return true;
+            }
         }
-        else if(k>c){
-            b--;
-        }
-        else{
-            return true;
-        }
-       }
 
-
-       return false;
-
+        return false;
     }
 };
