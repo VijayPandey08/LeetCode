b2dde7fa3b04cbb021a5e9ce4256e9be9503d236
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-19 01:45:19 +0530

    Time: 48 ms (68.34%) | Memory: 41.1 MB (66.75%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -2,36 +2,29 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
                             vector<int>& worker) {
-        // intution: need to maintain record of the max profit along with the
-        // difficulty so we use priority queue and take entry in pair
+        vector<pair<int, int>> v;
 
-        priority_queue<pair<int, int>> pq;
         for (int i = 0; i < profit.size(); i++) {
-            pq.push({profit[i], difficulty[i]});
+            v.push_back({difficulty[i], profit[i]});
         }
 
-        // this is important step to reverse the worker ability to do a work, so
-        // that we can pop entries from the pq which no worker can complete
-        sort(worker.rbegin(), worker.rend());
+        sort(v.begin(), v.end());
 
-        // basic initialisation
+        sort(worker.begin(), worker.end());
+
         int total_profit = 0;
-        int i = 0;
+        int max_profit = 0;
 
-        // normally tracking the max_profitability and adding it to the
-        // total_progitability
-        while (i < worker.size() && !pq.empty()) {
-            int max_profit = 0;
-            if (worker[i] < pq.top().second) {
-                pq.pop();
-            } else {
-                max_profit = max(max_profit, pq.top().first);
-                i++;
+        int j = 0;
+
+        for (int i = 0; i < worker.size(); i++) {
+            while (j < profit.size() && worker[i] >= v[j].first) {
+                max_profit = max(max_profit, v[j].second);
+                j++;
             }
             total_profit += max_profit;
         }
 
-        // return answer at last
         return total_profit;
     }
 };
