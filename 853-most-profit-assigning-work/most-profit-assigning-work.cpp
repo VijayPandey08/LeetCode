271c1b0fe52543fc9365877ba9058be3a8d313a8
Vijay Pandey <vijaypandey0825@gmail.com>
2024-06-19 01:36:24 +0530

    Time: 83 ms (25.31%) | Memory: 44.6 MB (21.64%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        priority_queue<pair<int,int>> pq;
-        for(int i=0; i<profit.size(); i++){
-            pq.push({profit[i],difficulty[i]});
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
+                            vector<int>& worker) {
+        // intution: need to maintain record of the max profit along with the
+        // difficulty so we use priority queue and take entry in pair
+
+        priority_queue<pair<int, int>> pq;
+        for (int i = 0; i < profit.size(); i++) {
+            pq.push({profit[i], difficulty[i]});
         }
 
-        // sort(pq.begin(),pq.end());
+        // this is important step to reverse the worker ability to do a work, so
+        // that we can pop entries from the pq which no worker can complete
+        sort(worker.rbegin(), worker.rend());
 
-        sort(worker.rbegin(),worker.rend());
+        // basic initialisation
+        int total_profit = 0;
+        int i = 0;
 
-
-         int total_profit=0;
-            int i=0;
-            while(i<worker.size() && !pq.empty()){
-            int max_profit=0;
-                  if(worker[i]<pq.top().second){
-                    pq.pop();
-                  }
-                  else{
-                    max_profit=max(max_profit,pq.top().first);
-                    i++;
-                  }
-            total_profit+=max_profit;
+        // normally tracking the max_profitability and adding it to the
+        // total_progitability
+        while (i < worker.size() && !pq.empty()) {
+            int max_profit = 0;
+            if (worker[i] < pq.top().second) {
+                pq.pop();
+            } else {
+                max_profit = max(max_profit, pq.top().first);
+                i++;
+            }
+            total_profit += max_profit;
         }
+
+        // return answer at last
         return total_profit;
     }
 };
