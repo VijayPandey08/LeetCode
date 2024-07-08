32c8d2152a9dba654fca2196458059856260b5c9
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-08 23:19:40 +0530

    Time: 23 ms (25.38%) | Memory: 25.7 MB (9.19%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -3,18 +3,35 @@ public:
     int findTheWinner(int n, int k) {
         // there are mainly three approaches to solve this question
 
-        //--------approach 1 ------------//
-        vector<int> v;
+ //         //--------approach 1 ------------// (using vector<int>)
+        // vector<int> v;
+        // for (int i = 1; i <= n; i++) {
+        //     v.push_back(i);
+        // }
+        // int index = 0;
+        // while (v.size() > 1) {
+        //     index = index + k - 1;
+        //     index = (index % v.size());
+        //     v.erase(v.begin() + index);
+        // }
+
+        // return v[0];
+
+        //-------approach 2------------// (using queue)
+        queue<int> q;
         for (int i = 1; i <= n; i++) {
-            v.push_back(i);
-        }
-        int index = 0;
-        while (v.size() > 1) {
-            index = index + k - 1;
-            index = (index % v.size());
-            v.erase(v.begin() + index);
+            q.push(i);
         }
 
-        return v[0];
+        while (q.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                int temp = q.front();
+                q.pop();
+                q.push(temp);
+            }
+            q.pop();
+        }
+
+        return q.front();
     }
 };
