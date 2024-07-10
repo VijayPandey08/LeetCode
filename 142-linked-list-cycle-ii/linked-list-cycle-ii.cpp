e3479e82cfe55c2efb8ff752a13f582257e1e46b
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-10 16:04:48 +0530

    Time: 10 ms (28.33%) | Memory: 10.2 MB (43.45%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -1,39 +1,35 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *   int val;
- *   ListNode * next;
- *   ListNode(int x) : val(x), next(NULL) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
  */
-class Solution
-{
-    public:
-        ListNode* detectCycle(ListNode *head)
-        {
-            ListNode *slow = head;
-            ListNode *fast = head;
-            while (fast != NULL)
-            {
-                fast = fast->next;
-                if (fast != NULL)
-                {
-                    fast = fast->next;
-                    slow = slow->next;
-                }
-                if (fast == slow)
-                {
-                    break;
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* temp = head;
+        ListNode* fast = temp;
+        ListNode* slow = temp;
+
+
+        while(fast!=NULL){
+            fast=fast->next;
+            if(fast!=NULL){
+                fast=fast->next;
+                slow=slow->next;
+                if(slow==fast){
+                    slow = temp;
+                    while(fast!=slow){
+                        fast=fast->next;
+                        slow=slow->next;
+                    }
+                    return slow;
                 }
             }
-           	if(fast==NULL) return NULL;  
-            slow = head;
-           	// slow aur fast ko ek ek step move krao
-            while (fast != slow)
-            {
-                fast = fast->next;
-                slow = slow->next;
-            }
-            return slow;
         }
+
+        return NULL;
+    }
 };
