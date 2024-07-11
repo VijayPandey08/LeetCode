265edf25cc3cf9e1522a39160ee063b182ef8e54
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-11 20:25:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (49.11%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -3,21 +3,50 @@ public:
     string reverseParentheses(string s) {
         //------approach 1-----------//
 
-        stack<int> reverseLen;
+        // stack<int> reverseLen;
 
-        string ans="";
+        // string ans="";
 
-        for(auto i:s){
-            if(i=='('){
-                reverseLen.push(ans.length());
+        // for(auto i:s){
+        //     if(i=='('){
+        //         reverseLen.push(ans.length());
+        //     }
+        //     else if(i==')'){
+        //         int len = reverseLen.top();
+        //         reverseLen.pop();
+        //         reverse(ans.begin()+len,ans.end());
+        //     }
+        //     else{
+        //         ans.push_back(i);
+        //     }
+        // }
+
+        // return ans;
+
+        //----------approach -2---------------//
+
+        stack<int> pairIdx;
+        vector<int> pair(s.length());
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                pairIdx.push(i);
+            } else if (s[i] == ')') {
+                int idx = pairIdx.top();
+                pairIdx.pop();
+                pair[i] = idx;
+                pair[idx] = i;
             }
-            else if(i==')'){
-                int len = reverseLen.top();
-                reverseLen.pop();
-                reverse(ans.begin()+len,ans.end());
-            }
-            else{
-                ans.push_back(i);
+        }
+
+        int direction = 1;
+        string ans = "";
+        for (int i = 0; i < s.length(); i += direction) {
+            if (s[i] == '(' || s[i] == ')') {
+                i = pair[i];
+                direction = -direction;
+            } else {
+                ans.push_back(s[i]);
             }
         }
 
