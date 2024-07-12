234a516e0c7e5b15089fbec52ae7b04fd59c271c
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-12 18:05:50 +0530

    Time: 69 ms (82.93%) | Memory: 21.4 MB (71.95%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,46 +1,59 @@
 class Solution {
 public:
+    // string gainStr(string &s, string str){
+    //     stack<char> st;
 
-string gainStr(string &s, string str){
-    stack<char> st;
+    //     for(int i=0; i<s.length(); i++ ){
+    //         if(!st.empty() && s[i]==str[1] && st.top()==str[0] ){
+    //             st.pop();
+    //         }
+    //         else{
+    //             st.push(s[i]);
+    //         }
+    //     }
+    // string temp="";
+    //     while(!st.empty()){
+    //         char ch=st.top();
+    //         temp.push_back(ch);
+    //         st.pop();
+    //     }
+    //     reverse(temp.begin(),temp.end());
+    //     return temp;
+    // }
 
-    for(int i=0; i<s.length(); i++ ){
-        if(!st.empty() && s[i]==str[1] && st.top()==str[0] ){
-            st.pop();
+    string gainStr(string& s, string str) {
+        int j = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            s[j++] = s[i];
+
+            if (j > 1 && s[j - 2] == str[0] && s[j - 1] == str[1]) {
+                j -= 2;
+            }
         }
-        else{
-            st.push(s[i]);
-        }
+
+        s.erase(s.begin() + j, s.end());
+
+        return s;
     }
 
-    string temp="";
-    while(!st.empty()){
-        char ch=st.top();
-        temp.push_back(ch);
-        st.pop();
-    }
-    reverse(temp.begin(),temp.end());
-    return temp;
-}
     int maximumGain(string s, int x, int y) {
         int n = s.length();
         int score = 0;
-        string maxStr = x>=y?"ab":"ba";
-        string minStr = x<y?"ab":"ba";
+        string maxStr = x >= y ? "ab" : "ba";
+        string minStr = x < y ? "ab" : "ba";
 
         //---------1st pass--------------//
-        string firstStr = gainStr(s,maxStr);
+        string firstStr = gainStr(s, maxStr);
         int len = firstStr.length();
-        int pairs = (n-len)/2;
-        score+=pairs*max(x,y);
+        int pairs = (n - len) / 2;
+        score += pairs * max(x, y);
 
         //---------2nd pass------------//
-        string secondStr =gainStr(firstStr,minStr);
+        string secondStr = gainStr(firstStr, minStr);
         int len2 = secondStr.length();
-        pairs =(len-len2)/2;
-        score+=pairs*min(x,y);
-
-
+        pairs = (len - len2) / 2;
+        score += pairs * min(x, y);
 
         return score;
     }
