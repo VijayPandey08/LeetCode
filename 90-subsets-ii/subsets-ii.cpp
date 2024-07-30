5468970e7a790b11c713e365e7218376213b54c9
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-31 01:34:20 +0530

    Time: 3 ms (56.92%) | Memory: 16 MB (10.13%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -9,7 +9,7 @@
 //     }
 //     temp.push_back(nums[index]);
 //     solve(nums,n,index+1,temp);
-//     temp.pop_back();
+    
 //     solve(nums,n,index+1,temp);
 
 // }
@@ -20,7 +20,7 @@
 //         int index = 0;
 
 //         solve(nums,n,index,temp);
-//         sort(ans.begin(),ans.end());
+        
 //         set<vector<int>> s;
 //         for(auto i:ans){
 //             s.insert(i);
@@ -34,34 +34,69 @@
 //     }
 // };
 
+#include <vector>
+#include <set>
+#include <algorithm>
 
 class Solution {
 public:
-    vector<vector<int>> ans;
+    std::vector<std::vector<int>> ans;
 
-    void solve(vector<int>& nums, int n, int index, vector<int> temp) {
-        if(index >= n) {
+    void solve(std::vector<int>& nums, int n, int index, std::vector<int> temp) {
+        if (index >= n) {
             ans.push_back(temp);
             return;
         }
-        solve(nums, n, index + 1, temp);
         temp.push_back(nums[index]);
+        solve(nums, n, index + 1, temp);
+        temp.pop_back(); // Backtrack
         solve(nums, n, index + 1, temp);
     }
 
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        ans.clear();  // Clear previous results
-        sort(nums.begin(), nums.end());  // Sort the input to handle duplicates
-        vector<int> temp;
+    std::vector<std::vector<int>> subsetsWithDup(std::vector<int>& nums) {
+        ans.clear(); // Clear previous results
+        std::sort(nums.begin(), nums.end()); // Sort the input to handle duplicates
+        std::vector<int> temp;
         int n = nums.size();
         int index = 0;
 
         solve(nums, n, index, temp);
-
-        // Remove duplicates
-        sort(ans.begin(), ans.end());
-        ans.erase(unique(ans.begin(), ans.end()), ans.end());
-
-        return ans;
+        
+        std::set<std::vector<int>> s(ans.begin(), ans.end()); // Use set to remove duplicates
+        std::vector<std::vector<int>> result(s.begin(), s.end()); // Convert set to vector
+        
+        return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> ans;
+
+//     void solve(vector<int>& nums, int n, int index, vector<int> temp) {
+//         if(index >= n) {
+//             ans.push_back(temp);
+//             return;
+//         }
+//         solve(nums, n, index + 1, temp);
+//         temp.push_back(nums[index]);
+//         solve(nums, n, index + 1, temp);
+//     }
+
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         ans.clear();  // Clear previous results
+//         sort(nums.begin(), nums.end());  // Sort the input to handle duplicates
+//         vector<int> temp;
+//         int n = nums.size();
+//         int index = 0;
+
+//         solve(nums, n, index, temp);
+
+//         // Remove duplicates
+//         sort(ans.begin(), ans.end());
+//         ans.erase(unique(ans.begin(), ans.end()), ans.end());
+
+//         return ans;
+//     }
+// };
