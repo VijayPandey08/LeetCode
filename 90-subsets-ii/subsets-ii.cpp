8460b8e6d844ab4b56609e39b0755a2cac711c4d
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-31 01:36:40 +0530

    Time: 4 ms (48.20%) | Memory: 16.2 MB (6.89%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,74 +1,67 @@
-// class Solution {
-// public:
-// vector<vector<int>> ans;
-
-// void solve(vector<int>& nums, int n, int index, vector<int> temp){
-//     if(index>=n){
-//         ans.push_back(temp);
-//         return;
-//     }
-//     temp.push_back(nums[index]);
-//     solve(nums,n,index+1,temp);
-    
-//     solve(nums,n,index+1,temp);
-
-// }
-
-//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-//         vector<int> temp;
-//         int n =nums.size();
-//         int index = 0;
-
-//         solve(nums,n,index,temp);
-        
-//         set<vector<int>> s;
-//         for(auto i:ans){
-//             s.insert(i);
-//         }
-//         vector<vector<int>> result;
-//         for(auto i:s){
-//             result.push_back(i);
-//         }
-//         return result;
-
-//     }
-// };
-
-#include <vector>
-#include <set>
-#include <algorithm>
-
 class Solution {
 public:
-    std::vector<std::vector<int>> ans;
+    vector<vector<int>> ans;
 
-    void solve(std::vector<int>& nums, int n, int index, std::vector<int> temp) {
+    void solve(vector<int>& nums, int n, int index, vector<int> temp) {
         if (index >= n) {
             ans.push_back(temp);
             return;
         }
         temp.push_back(nums[index]);
         solve(nums, n, index + 1, temp);
-        temp.pop_back(); // Backtrack
+        temp.pop_back();
         solve(nums, n, index + 1, temp);
     }
 
-    std::vector<std::vector<int>> subsetsWithDup(std::vector<int>& nums) {
-        ans.clear(); // Clear previous results
-        std::sort(nums.begin(), nums.end()); // Sort the input to handle duplicates
-        std::vector<int> temp;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> temp;
         int n = nums.size();
         int index = 0;
-
+        sort(nums.begin(), nums.end());
         solve(nums, n, index, temp);
-        
-        std::set<std::vector<int>> s(ans.begin(), ans.end()); // Use set to remove duplicates
-        std::vector<std::vector<int>> result(s.begin(), s.end()); // Convert set to vector
-        
+
+        set<vector<int>> s(ans.begin(), ans.end());
+
+        vector<vector<int>> result(s.begin(), s.end());
+
         return result;
     }
 };
 
+// #include <vector>
+// #include <set>
+// #include <algorithm>
+
+// class Solution {
+// public:
+//     std::vector<std::vector<int>> ans;
+
+//     void solve(std::vector<int>& nums, int n, int index, std::vector<int>
+//     temp) {
+//         if (index >= n) {
+//             ans.push_back(temp);
+//             return;
+//         }
+//         temp.push_back(nums[index]);
+//         solve(nums, n, index + 1, temp);
+//         temp.pop_back(); // Backtrack
+//         solve(nums, n, index + 1, temp);
+//     }
+
+//     std::vector<std::vector<int>> subsetsWithDup(std::vector<int>& nums) {
+//         ans.clear(); // Clear previous results
+//         std::sort(nums.begin(), nums.end()); // Sort the input to handle
+//         duplicates std::vector<int> temp; int n = nums.size(); int index = 0;
+
+//         solve(nums, n, index, temp);
+
+//         std::set<std::vector<int>> s(ans.begin(), ans.end()); // Use set to
+//         remove duplicates std::vector<std::vector<int>> result(s.begin(),
+//         s.end()); // Convert set to vector
+
+//         return result;
+//     }
+// };
 
 // class Solution {
 // public:
@@ -86,10 +79,8 @@ public:
 
 //     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 //         ans.clear();  // Clear previous results
-//         sort(nums.begin(), nums.end());  // Sort the input to handle duplicates
-//         vector<int> temp;
-//         int n = nums.size();
-//         int index = 0;
+//         sort(nums.begin(), nums.end());  // Sort the input to handle
+//         duplicates vector<int> temp; int n = nums.size(); int index = 0;
 
 //         solve(nums, n, index, temp);
 
