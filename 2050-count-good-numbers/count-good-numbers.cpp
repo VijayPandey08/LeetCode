3506095fd7f90965a32d86ca853fcea2810b4b0b
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-31 03:05:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (21.98%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
+int MOD;
+    long long modPow(long long base, long long exp) {
+        long long result = 1;
+        while (exp > 0) {
+            if (exp & 1)
+                result = (result * base) % MOD;
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return result;
+    };
     int countGoodNumbers(long long n) {
-        const int MOD = 1e9 + 7;
-        
-        auto modPow = [&](long long base, long long exp) {
-            long long result = 1;
-            while (exp > 0) {
-                if (exp & 1) result = (result * base) % MOD;
-                base = (base * base) % MOD;
-                exp >>= 1;
-            }
-            return result;
-        };
-        
-        long long even = (n + 1) / 2;  // Number of even positions
-        long long odd = n / 2;         // Number of odd positions
-        
+        MOD = 1e9 + 7;
+
+        long long even = (n + 1) / 2; // Number of even positions
+        long long odd = n / 2;        // Number of odd positions
+
         return (modPow(5, even) * modPow(4, odd)) % MOD;
     }
 };
