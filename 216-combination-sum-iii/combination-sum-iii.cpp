72875b71bf89931f9ed584f7256a11e7568e7fa8
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-31 06:41:07 +0530

    Time: 7 ms (6.60%) | Memory: 14.5 MB (5.02%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,70 +1,82 @@
-// class Solution {
-// public:
-//     vector<vector<int>> ans;
-//     int len;
-
-//     void solve(vector<int>& nums, int index, int k, int target,
-//                vector<int> temp) {
-//         if (temp.size() == k && target == 0) {
-//             ans.push_back(temp);
-//             return;
-//         }
-//         if (index >= len) {
-//             return;
-//         }
-//         temp.push_back(nums[index]);
-//         solve(nums, index + 1, k, target - nums[index], temp);
-//         temp.pop_back();
-//         solve(nums, index + 1, k, target, temp);
-//     }
-
-//     vector<vector<int>> combinationSum3(int k, int n) {
-//         vector<int> nums;
-//         for (int i = 1; i <= 9; i++) {
-//             nums.push_back(i);
-//         }
-//         len = nums.size();
-//         int index = 0;
-
-//         vector<int> temp;
-//         solve(nums, index, k, n, temp);
-//         return ans;
-//     }
-// };
-
-
-
 class Solution {
 public:
     vector<vector<int>> ans;
     int len;
 
-    void solve(int index, int k, int target,
+    // ------------------ approach 1 ----------------------//
+    // k numbers uthane h 1 se 9 ke beech aur target banana h
+    // intution based chale 1 se 9 ka nums array bana dia, aur base case dekh
+    // lia ki jb exactly k numbers ho aur target zero ho jaae tab hi add krnaa h
+    // classic backtrcking
+
+    void solve(vector<int>& nums, int index, int k, int target,
                vector<int> temp) {
+
+        // base case
         if (temp.size() == k && target == 0) {
             ans.push_back(temp);
             return;
         }
-        if (index > len) {
+        if (index >= len) {
             return;
         }
-        temp.push_back(index);
-        solve(index + 1, k, target - index, temp);
-        temp.pop_back();
-        solve(index + 1, k, target, temp);
+
+        temp.push_back(nums[index]); // inclusion
+        solve(nums, index + 1, k, target - nums[index], temp);
+
+        temp.pop_back(); // backtracking exclusion
+        solve(nums, index + 1, k, target, temp);
     }
 
     vector<vector<int>> combinationSum3(int k, int n) {
-        // vector<int> nums;
-        // for (int i = 1; i <= 9; i++) {
-        //     nums.push_back(i);
-        // }
-        // len = nums.size();
-        len = 9;
-        int index = 1;
+        //
+        vector<int> nums;
+        for (int i = 1; i <= 9; i++) {
+            nums.push_back(i);
+        }
+        len = nums.size();
+        int index = 0;
 
         vector<int> temp;
-        solve(index, k, n, temp);
+        solve(nums, index, k, n, temp);
         return ans;
     }
 };
+
+// ------------- approach 2 ---------------------//
+// variables zada ho rhe the toh isme kam variables use kre, jab numbers uthane
+// 1 se 9 ke beech h, aur vo bhi at most ek baar toh nums array na bana ke, ek
+// hi variable m kaam kr dia, ya toh index lenge ya nhi lenge, baki sb at it is
+
+// class Solution {
+// public:
+//     vector<vector<int>> ans;
+//     int len;
+
+//     void solve(int index, int k, int target, vector<int> temp) {
+
+//         // base case
+//         if (temp.size() == k && target == 0) {
+//             ans.push_back(temp);
+//             return;
+//         }
+//         if (index > len) {
+//             return;
+//         }
+
+//         temp.push_back(index); // inclusion
+//         solve(index + 1, k, target - index, temp);
+
+//         temp.pop_back(); // backtracking exclusion
+//         solve(index + 1, k, target, temp);
+//     }
+
+//     vector<vector<int>> combinationSum3(int k, int n) {
+//         len = 9;
+//         int index = 1;
+
+//         vector<int> temp;
+//         solve(index, k, n, temp);
+//         return ans;
+//     }
+// };
