f125939440885dd93ed01f2cf808c0f9fce9c1ef
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-31 07:35:56 +0530

    Time: 3 ms (45.86%) | Memory: 26.5 MB (5.61%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
-int n;
+    // sexy backtracking + dp ka question
+    int n;
 
-int solve(int index, int target, vector<int>&nums, vector<vector<int>>& dp){
-    if(target==0){
-        return 1;
-    }
-    if(index>=n || target<0){
-        return 0;
-    }
-    if(dp[index][target]!=-1){
-        return dp[index][target];
+    int solve(int index, int target, vector<int>& nums,
+              vector<vector<int>>& dp) {
+        // base case
+        if (target == 0) {
+            return 1;
+        }
+        if (index >= n || target < 0) {
+            return 0;
+        }
+
+        // memoisation
+        if (dp[index][target] != -1) {
+            return dp[index][target];
+        }
+
+        // // backtracking , take/not_take
+        // int take = solve(0,target-nums[index],nums,dp);
+        // int not_take = solve(index+1,target,nums,dp);
+        // // storing in dp
+        // return dp[index][target]=take+not_take;
+
+        //--------- can also be done through one loop ----//
+        int result = 0;
+        for (int i = index; i < n; i++) {
+            int take = solve(0, target - nums[i], nums, dp);
+            result += take;
+        }
+        return dp[index][target] = result;
     }
 
-    int take = solve(0,target-nums[index],nums,dp);
-    int not_take = solve(index+1,target,nums,dp);
-
-    return dp[index][target]=take+not_take;
-}
     int combinationSum4(vector<int>& nums, int target) {
         n = nums.size();
-        int index =0;
+        int index = 0;
 
-vector<vector<int>> dp(201,vector<int>(1001,-1));
+        vector<vector<int>> dp(201, vector<int>(1001, -1));
 
-        return solve(index,target,nums,dp);
-        
+        return solve(index, target, nums, dp);
     }
 };
