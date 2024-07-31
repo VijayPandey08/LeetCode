7446f03718a8bb0f0e6b3e8b48ebd8ffcaf2250c
Vijay Pandey <vijaypandey0825@gmail.com>
2024-07-31 06:05:22 +0530

    Time: 7 ms (6.44%) | Memory: 14.3 MB (16.29%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-vector<vector<int>> ans;
-int n;
+    vector<vector<int>> ans;
+    int n;
 
-void solve(vector<int>& nums, int index, vector<int> temp){
-    if(index>=n){
-        ans.push_back(temp);
-        return;
+    void solve(vector<int>& nums, int index, vector<int> temp) {
+        // base case
+        if (index >= n) {
+            ans.push_back(temp);
+            return;
+        }
+
+        // nums unique h, toh duplicate ki tension nhi
+        // ya toh lo, ya nhi lo, baaat khtm
+
+        temp.push_back(nums[index]); // inclusion
+        solve(nums, index + 1, temp);
+
+        temp.pop_back(); // backtracking
+        solve(nums, index + 1, temp);
     }
-
-    temp.push_back(nums[index]);
-    solve(nums,index+1,temp);
-    temp.pop_back();
-    solve(nums,index+1,temp);
-}
     vector<vector<int>> subsets(vector<int>& nums) {
         n = nums.size();
-        int index=0;
+        int index = 0;
         vector<int> temp;
 
-        solve(nums,index,temp);
+        solve(nums, index, temp);
         return ans;
-
     }
 };
