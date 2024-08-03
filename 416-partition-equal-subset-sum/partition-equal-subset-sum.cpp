b8749ecac3c91efb5620a1bc63028cc737aaaa11
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-04 00:51:36 +0530

    Time: 155 ms (53.68%) | Memory: 20.9 MB (57.43%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    int dp[201][10000];
     bool solve(vector<int>& nums, int target, int index) {
         if (target == 0) {
             return true;
@@ -7,29 +9,12 @@ public:
         if (index >= nums.size() || target < 0) {
             return false;
         }
-
+        if(dp[index][target] != -1) return dp[index][target];
         bool take = solve(nums, target - nums[index], index + 1);
         bool not_take = solve(nums, target, index + 1);
-        return take | not_take;
+        return dp[index][target] =take | not_take;
     }
-    bool solvememo(vector<int>& nums, int target, int index,vector<vector<int>> &dp) {
-        if (target == 0) {
-            return true;
-        }
-        if (index >= nums.size() || target < 0) {
-            return false;
-        }
-        if(dp[index][target]!=-1){
-            return dp[index][target];
-        }
-
-        bool take = solvememo(nums, target - nums[index], index + 1,dp);
-        bool not_take = solvememo(nums, target, index + 1,dp);
-        dp[index][target] =(take | not_take);
-
-        return dp[index][target];
-    }
-
+    
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0ll);
         if (sum & 1)
@@ -38,8 +23,7 @@ public:
 
         int index = 0;
         int n=nums.size();
-        vector<vector<int>> dp(n+5,vector<int>(target+1,-1));
-
-        return solvememo(nums, target, index,dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(nums, target, index);
     }
 };
