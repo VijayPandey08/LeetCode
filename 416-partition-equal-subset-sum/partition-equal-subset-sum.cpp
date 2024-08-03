2c731773a0029b64a9e8b0127fb236524aa8ebe1
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-04 00:45:50 +0530

    Time: 152 ms (54.74%) | Memory: 102.6 MB (16.73%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,51 +1,45 @@
 class Solution {
 public:
-
-// bool solve(vector<int>& nums,int index, int target, vector<vector<int>> &dp){
-// // recursion
-
-//     if(index>=nums.size()){
-//         if(target==0) return true;
-//         return false;
-//     } 
-//     if(target<0) return false;
-//     if(target==0) return true;
-
-//     if(dp[index][target]!=-1){
-//         return dp[index][target];
-//     }
-
-//     bool include = solve(nums,index+1,target-nums[index],dp);
-//     bool exclude = solve(nums,index+1,target,dp);
-
-//     return dp[index][target]=(include | exclude);
-
-// }
-    bool canPartition(vector<int>& nums) {
-       int n = nums.size();
-       int sum = accumulate(nums.begin(),nums.end(),0ll);
-       if(sum&1) return false;
-       int target = sum /2;
-        vector<vector<bool>> dp(nums.size()+1,vector<bool>(target+1,false));
-    //    cout<<target<<endl;
-    //    return solve(nums,0,target,dp);
-    for(int i=0; i<dp.size(); i++){
-        dp[i][0]=true;
-    }
-    for(int i=1; i<dp.size(); i++){
-        for(int j=1; j<target+1; j++){
-               if(j>=nums[i-1]){
-                dp[i][j]=dp[i-1][j] | dp[i-1][j-nums[i-1]];
-               }
-               else{
-                 dp[i][j] = dp[i-1][j];
-               }
+    bool solve(vector<int>& nums, int target, int index) {
+        if (target == 0) {
+            return true;
         }
+        if (index >= nums.size() || target < 0) {
+            return false;
+        }
+
+        bool take = solve(nums, target - nums[index], index + 1);
+        bool not_take = solve(nums, target, index + 1);
+        return take | not_take;
     }
-    return dp[nums.size()][target];
+    bool solvememo(vector<int>& nums, int target, int index,vector<vector<int>> &dp) {
+        if (target == 0) {
+            return true;
+        }
+        if (index >= nums.size() || target < 0) {
+            return false;
+        }
+        if(dp[index][target]!=-1){
+            return dp[index][target];
+        }
 
+        bool take = solvememo(nums, target - nums[index], index + 1,dp);
+        bool not_take = solvememo(nums, target, index + 1,dp);
+        dp[index][target] =(take | not_take);
 
+        return dp[index][target];
+    }
 
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0ll);
+        if (sum & 1)
+            return false;
+        int target = sum >> 1;
 
+        int index = 0;
+        int n=nums.size();
+        vector<vector<int>> dp(n+5,vector<int>(target+1,-1));
+
+        return solvememo(nums, target, index,dp);
     }
 };
