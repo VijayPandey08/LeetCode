15eb60c3aeb1d47356f41cca35885b319533b942
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-06 18:10:42 +0530

    Time: 116 ms (29.08%) | Memory: 29.3 MB (5.55%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -6,29 +6,24 @@ public:
             m[i]++;
         }
         int ans = 0;
-        int count = 1;
 
         // for(auto i:m){
         //     cout<<i.first<<" "<<i.second;
         // cout<<endl;
         // }
         vector<int> vec;
-        for (auto i : m) {
-            
+        for (auto i : m) {   
             vec.push_back(i.second);
-            // p.make_pair({i.second,i.first});
         }
 
         sort(vec.rbegin(), vec.rend());
-
-        // for (auto i : vec) {
-        //     cout << i.first << " " << i.second;
-        //     cout << endl;
-        // }
-// int count=0;
-
-        for(int i = 0;i<vec.size();i++){
-            ans+= vec[i]* ((i+1+7)/8);
+        
+        int count = 1;
+        int val = 1;
+        for(auto i:vec){
+            ans+= i*val;
+            if(count%8 == 0) val++;
+            count++;
         }
 
 
