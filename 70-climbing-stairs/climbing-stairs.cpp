ad6dd95d0ac1be61b1dde02d969e01947a9bc6f3
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-09 03:20:01 +0530

    Time: 3 ms (29.51%) | Memory: 7.7 MB (6.37%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -13,6 +13,22 @@ public:
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
 
-        return solve(n, dp);
+        vector<int> table(n+1,-1);
+        table[0]=1;
+        table[1]=1;
+
+        for(int i=2; i<table.size(); i++){
+            table[i] = table[i-1] + table[i-2];
+        }
+
+        return table[n];
+
+        // return solve(n, dp);
     }
+
+
+
+
+
+
 };
