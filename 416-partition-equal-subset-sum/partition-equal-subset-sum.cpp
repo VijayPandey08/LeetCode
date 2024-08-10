56af05271489c3d90401ed87844794c998cef3f8
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-11 05:16:08 +0530

    Time: 557 ms (5.03%) | Memory: 102.3 MB (16.59%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -21,27 +21,47 @@ public:
         int target = sum >> 1;
         int n = nums.size();
         int dpp[n+1][target+1];
+        // int curr[target+1];
+        // int prev[target+1]={0};
+        vector<int> curr(target+1,0);
+        vector<int> prev(target+1,0);
+        prev[0]= 1;
 
-        for(int i=0; i<n+1; i++){
-            for(int j=0; j<target+1; j++){
-                if(i==0 && j==0){
-                    dpp[i][j] =1;
-                }
-                else if(i==0){
-                      dpp[i][j] = 0;
-                }
-                else if(j==0){
-                    dpp[i][j] =1;
-
-                }
-                else{
-                        dpp[i][j] = dpp[i-1][j];   // not take
-                        if(j-nums[i-1] >=0) dpp[i][j] |=  dpp[i-1][j-nums[i-1]]; //take
-                }
+        for(int i = 1;i<n+1;i++){
+            for(int j = 0;j<target+1;j++){
+                curr[j] = prev[j];
+                if(j-nums[i-1]>=0) curr[j] |= prev[j-nums[i-1]];
+            }
+            prev = curr;
+            for(int j = 0 ;j<target+1;j++){
+                curr[j] = 0;
             }
         }
+        return prev[target];
 
-        return dpp[n][target];
+
+        
+
+        // for(int i=0; i<n+1; i++){
+        //     for(int j=0; j<target+1; j++){
+        //         if(i==0 && j==0){
+        //             dpp[i][j] =1;
+        //         }
+        //         else if(i==0){
+        //               dpp[i][j] = 0;
+        //         }
+        //         else if(j==0){
+        //             dpp[i][j] =1;
+
+        //         }
+        //         else{
+        //                 dpp[i][j] = dpp[i-1][j];   // not take
+        //                 if(j-nums[i-1] >=0) dpp[i][j] |=  dpp[i-1][j-nums[i-1]]; //take
+        //         }
+        //     }
+        // }
+
+        // return dpp[n][target];
 /*
 T F F F F
 T
