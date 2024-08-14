27b09c08790f1cd52213752ee7e714557c353c8c
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-15 01:17:53 +0530

    Time: 4 ms (31.67%) | Memory: 9.5 MB (28.96%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
+        vector<int> dp(nums.size(), -1);
         dp[0] = nums[0];
-        if(nums.size()==1) return nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        if (nums.size() == 1) return nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
-        for(int i=2; i<nums.size(); i++){
-            dp[i] = max(dp[i-1],(dp[i-2]+nums[i]));
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1], (dp[i - 2] + nums[i]));
         }
-for(auto i:dp){
-    cout<<i<<" ";
-}
-cout<<endl;
-        return dp[nums.size()-1];
+
+        int prev_2 = nums[0];
+        if (nums.size() == 1) return nums[0];
+        int prev_1 = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            int curr = max(prev_1, (prev_2 + nums[i]));
+            prev_2 = prev_1;
+            prev_1 = curr;
+        }
+
+        // return dp[nums.size() - 1];
+        return prev_1;
     }
 };
