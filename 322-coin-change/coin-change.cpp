ecdbac5244677947006c6c08be218db88a472da4
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-15 02:03:12 +0530

    Time: 16 ms (94.82%) | Memory: 16.4 MB (71.84%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount, vector<int> &dp){
-        // base case agar amount hi zero ho gya toh
-        if(amount==0){
-            return 0;
-        }
-
-        if(dp[amount]!=-1){
-            return dp[amount];
-        }
-        // minimum ans chahiye toh isi mein store krke rkhenge
-        int mini=INT_MAX;
-        
-        // sare coins select kr ske  islie loop chalaenge
-        for(int i=0; i<coins.size(); i++){
-            // kisi coin ko tab hi select krenge jab vo amount se chota ho
-           if(amount>=coins[i]){
-            // har coin ke lie ans
-            int recursionkaans = solve(coins,amount-coins[i],dp);
-            // is ans ko tab hi consider krenge jab vo int max na ho
-            if(recursionkaans!=INT_MAX){
-            int ans = 1 + recursionkaans;
-            mini=min(mini,ans); // minimun maintain kr rhe h
-            }
-           }
-        }
-        dp[amount] = mini;
-        return dp[amount];
-
-    }
     int coinChange(vector<int>& coins, int amount) {
-        int n=amount +1;
-        vector<int> dp(n,-1);
-        int temp = solve(coins,amount,dp);
-        if(temp == INT_MAX ) return -1;  // int_max reh gya matlab amount nahi ban paya ese mein -1 hi return krna h
-        else return temp;
-    
+        vector<int> dp(amount+1,INT_MAX);
+
+        dp[0] = 0;
+
+        for(int i=1; i<amount+1; i++){
+            int mini = INT_MAX;
+            for(auto j:coins){
+                if(i>=j){
+                   mini = min(mini,dp[i-j]);
+                }
+            }
+            if(mini!=INT_MAX){
+                dp[i] = 1 +mini;
+            }
+        }
+
+        if(dp[amount]==INT_MAX) return -1;
+        return dp[amount];
     }
 };
