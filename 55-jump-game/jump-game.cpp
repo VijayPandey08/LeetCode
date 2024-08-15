762d9c232b098b03c9b20f02e44d4bb064afa5c9
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-16 01:28:12 +0530

    Time: 2092 ms (5.02%) | Memory: 51.6 MB (14.06%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,28 +1,24 @@
-class Solution
-{
-    public:
-        bool canJump(vector<int> &nums)
-        {
-         int n =nums.size();
-         vector<int> v;
-        
-        if(n==1) return true;
-        
-         for(int i=n-2; i>=0; i--){
-             if(nums[i]==0){
-                 v.push_back(i);
-             }
-             else{
-                 while(v.size()>0 && v.back()<nums[i]+i){
-                     v.pop_back();
-                 }
-             }
-         }
-        if(v.size()>0) return false;
-        return true;
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
 
-    
+        if (nums.size() == 1)
+            return true;
 
+        vector<bool> dpp(nums.size(), false);
+        int n = dpp.size();
+        dpp[n - 1] = true;
 
+        for (int i = n - 2; i >= 0; i--) {
+
+            for (int j = 1; j <= nums[i] && i + j < n; j++) {
+                dpp[i] = (dpp[i] | dpp[i + j]);
+                if(dpp[i]) break;
+            }
         }
+
+        return dpp[0];
+
+
+    }
 };
