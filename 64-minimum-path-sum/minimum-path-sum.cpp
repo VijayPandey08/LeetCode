a800d7dbfbeb566f95caa17da2590537a5e1e520
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-16 04:18:15 +0530

    Time: 7 ms (61.18%) | Memory: 12.5 MB (86.01%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -23,11 +23,46 @@ int solve(vector<vector<int>>& grid, int n, int m, int i, int j){
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(i==0 && j==0){
+                    
+                }
+                else if(i==0){
+                      grid[i][j] += grid[i][j-1]; 
+                }
+                else if(j==0){
+                      grid[i][j] += grid[i-1][j];
+                }
+                else{
+                    grid[i][j] += min(grid[i-1][j],grid[i][j-1]);
+                }
+            }
+        }
+
+        return grid[n-1][m-1];
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        // --------------- memoisation ------------//
+
         int i=0;
         int j=0;
 memset(dp,-1,sizeof(dp));
 
 
         return solve(grid,n,m,i,j);
+
     }
 };
