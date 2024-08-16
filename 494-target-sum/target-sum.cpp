8fca8aee435dd2824765ff39cd7589a7b697f36f
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-17 03:54:50 +0530

    Time: 8 ms (65.19%) | Memory: 11.5 MB (65.29%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int index, int temp, vector<vector<int>>& dp) {
-        if (temp == 0 && index >= nums.size()) {
-            return 1;
-        }
-        if (temp < 0 || index >= nums.size()) {
-            return 0;
-        }
 
-        if (dp[index][temp] != -1) {
-            return dp[index][temp];
-        }
-
-        int a = solve(nums, index + 1, temp - nums[index],dp);
-        int b = solve(nums, index + 1, temp,dp);
-        return dp[index][temp] = a + b;
+int dp[25][5000];
+int solve(vector<int> &nums, int target, int index){
+    if(index==nums.size()){
+        return ((target==0)?1:0);
     }
 
+    if(dp[index][target + 2000]!=-1){
+             return dp[index][target+2000];
+    }
+
+    int a = solve(nums,target-nums[index],index+1);
+    int b = solve(nums,target+nums[index],index+1);
+
+    return dp[index][target+2000]=  a+b;
+    
+}
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        
 
-        int index = 0;
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<int>> dp(25, vector<int>(1005, -1));
-
-        // basically kuch positive value(x) + kuch negative values(y) mila ke
-        // target milega x + (-y) = target ,  yeh sala negative humne kia h x +
-        // y = sum , yeh original jo nums ka sum hota bina koi fer badal
-
-        int temp = (sum + target) / 2; // master stroke by jayant maurya
-        // positive assign krne ke lie kitni values h num ke pss yeh nikal lo
-
-        // int temp = ( sum - target)/2;
-        // negative sign vali kitni values hongi yeh find krlo
-
-        if((sum+target)&1) return 0;
-
-        return solve(nums, index, temp, dp);
+        memset(dp,-1,sizeof(dp));
+        int index=0;
+        return solve(nums,target,index);
     }
 };
