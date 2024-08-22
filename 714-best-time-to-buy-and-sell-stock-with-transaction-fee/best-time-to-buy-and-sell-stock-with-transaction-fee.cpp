ebf77a63d03728020d5dc213f86c9e88156c6769
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-22 08:18:29 +0530

    Time: 76 ms (79.61%) | Memory: 57.7 MB (83.66%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -4,21 +4,25 @@ public:
         int old_buy = -prices[0];
         int old_sell = 0;
 
+        int new_buy = 0;
+        int new_sell = 0;
+        
         for (int i = 1; i < prices.size(); i++) {
-            int new_buy = 0;
-            int new_sell = 0;
-
+            // for buying new stock
             if (old_sell - prices[i] > old_buy) {
                 new_buy = old_sell - prices[i];
             } else {
                 new_buy = old_buy;
             }
+
+            // for selling a stock
             if (prices[i] - fee + old_buy > old_sell) {
                 new_sell = prices[i] - fee + old_buy;
             } else {
                 new_sell = old_sell;
             }
 
+            // updation after every iteration
             old_buy = new_buy;
             old_sell = new_sell;
         }
