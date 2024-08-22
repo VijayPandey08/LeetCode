ce011eb740fe58a6709b49ab6b4076e6a2e4d973
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-22 08:13:13 +0530

    Time: 75 ms (80.89%) | Memory: 57.5 MB (88.13%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int old_buy = -prices[0];
+        int old_sell = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            int new_buy = 0;
+            int new_sell = 0;
+
+            if (old_sell - prices[i] > old_buy) {
+                new_buy = old_sell - prices[i];
+            } else {
+                new_buy = old_buy;
+            }
+            if (prices[i] - fee + old_buy > old_sell) {
+                new_sell = prices[i] - fee + old_buy;
+            } else {
+                new_sell = old_sell;
+            }
+
+            old_buy = new_buy;
+            old_sell = new_sell;
+        }
+
+        return old_sell;
+    }
+};
