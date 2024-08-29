e2c1320ddb173c4bc86892423b7a1c337fe81fe1
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-30 02:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (9.78%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+
+        for (int i = 1; i < target[0]; i++) {
+            ans.push_back("Push");
+            ans.push_back("Pop");
+        }
+        ans.push_back("Push");
+
+        for (int i = 1; i < target.size(); i++) {
+            if (target[i] - target[i - 1] == 1) {
+                ans.push_back("Push");
+            } else {
+                for (int j = 0; j < target[i] - target[i - 1] - 1; j++) {
+                    ans.push_back("Push");
+                    ans.push_back("Pop");
+                }
+                ans.push_back("Push");
+            }
+        }
+        return ans;
+    }
+};
