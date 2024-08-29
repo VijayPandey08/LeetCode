7f86f1753cba95a7b209165e326f2b59f64096d1
Vijay Pandey <vijaypandey0825@gmail.com>
2024-08-30 02:21:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (43.91%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        vector<string> ans;
+        vector<string> ans;  // isme store hoga obviously
 
+// -------- pehle index ko alg se handle kr rhe h -------------//
         for (int i = 1; i < target[0]; i++) {
             ans.push_back("Push");
             ans.push_back("Pop");
         }
         ans.push_back("Push");
+
+
+// ---------- baki cases ke lie for loop --------------------//
 
         for (int i = 1; i < target.size(); i++) {
             if (target[i] - target[i - 1] == 1) {
