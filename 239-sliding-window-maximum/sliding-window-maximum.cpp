94800afa8d8ee6b819f4ab4ffdd1bf95a7b56733
Vijay Pandey <vijaypandey0825@gmail.com>
2024-09-07 01:02:52 +0530

    Time: 162 ms (79.12%) | Memory: 137.4 MB (67.15%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,46 +1,30 @@
-class Solution
-{
-    public:
-        vector<int> maxSlidingWindow(vector<int> &nums, int k)
-        {
-            vector<int> ans;
-            deque<int> dq;
-           	///pehli window process kro
-            for (int i = 0; i < k; i++)
-            {
-                int element = nums[i];
-               	// jitne bhi queue k andar chote elements h remove kr do
-                while (!dq.empty() && element > nums[dq.back()])
-                {
-                    dq.pop_back();
-                }
-               	//now insert kardo
-                dq.push_back(i);
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;
+
+        for (int i = 0; i < k; i++) {
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
             }
-           	/// remaing window ko process kro
-           	// removal2 types
-           	// addition
-            for (int i = k; i < nums.size(); i++)
-            {
-               	//ans store kro
-                ans.push_back(nums[dq.front()]);
-               	// removal out of range
-                if (!dq.empty() && i - dq.front() >= k)
-                {
-                    dq.pop_front();
-                }
-               	// removal chote vale
-                int element = nums[i];
-                while (!dq.empty() && element > nums[dq.back()])
-                {
-                    dq.pop_back();
-                }
-               	//addition
-                dq.push_back(i);
-            }
-        
-           	//last winow ka answer store karna pdega
-            ans.push_back(nums[dq.front()]);
-            return ans;
+            dq.push_back(i);
         }
+
+        ans.push_back(nums[dq.front()]);
+
+        for (int i = k; i < nums.size(); i++) {
+
+            while (!dq.empty() && i - dq.front() >= k) {
+                dq.pop_front();
+            }
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            ans.push_back(nums[dq.front()]);
+        }
+
+        return ans;
+    }
 };
