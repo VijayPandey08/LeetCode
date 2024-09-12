d34ea16c17b211e8b7900f1f07f464507fa754a8
Vijay Pandey <vijaypandey0825@gmail.com>
2024-09-12 17:19:09 +0530

    Time: 3 ms (56.82%) | Memory: 14.2 MB (87.49%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -6,46 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-vector<int> ans;
+    void solve(TreeNode* root, vector<int>& ans, int depth) {
+        if (root == NULL) {
+            return;
+        }
 
+        if (depth == ans.size()) {
+            ans.push_back(root->val);
+        }
 
-void dfs(TreeNode* root, int level=1){
-    if(root==NULL) return;
-if(ans.size()<level){
-    ans.push_back(root->val);
-}
-    dfs(root->right,level+1);
-    dfs(root->left,level+1);
-}
+        if (root->right)
+            solve(root->right, ans, depth + 1);
+        if (root->left)
+            solve(root->left, ans, depth + 1);
+    }
+
     vector<int> rightSideView(TreeNode* root) {
-        //bfs
-        // if(root==NULL) return {};
-        // queue<TreeNode*> q;
-        // q.push(root);
-        
-        // while(q.size()>0){
-        //     int a = q.size();
-        //     for(int i=0; i<a; i++){
-        //         TreeNode* node = q.front();
-        //         q.pop();
-        //         if(i==a-1){
-        //             ans.push_back(node->val);
-        //         }
-        //        if(node->left) q.push(node->left);
-        //        if(node->right) q.push(node->right);
-        //     }
-        // }
-        // return ans;
-
-
-        //dfs
-        dfs(root);
+        // int left = 0;
+        // int right = 0;
+        int depth = 0;
+        vector<int> ans;
+        solve(root, ans, depth);
         return ans;
-
     }
 };
