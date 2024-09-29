e855af1cd4d8fb9399cc42930ec721931343bbee
Vijay Pandey <vijaypandey0825@gmail.com>
2024-09-30 03:41:43 +0530

    Time: 87 ms (5.18%) | Memory: 22 MB (30.81%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,37 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode* root){
-        if(root==NULL){
+    int height(TreeNode* root) {
+        if (root == NULL) {
             return 0;
         }
-        int leftheight = height(root->left);
-        int rightheight = height(root->right);
-        return max(leftheight,rightheight) + 1;
+
+        int left = 1 + height(root->left);
+        int right = 1 + height(root->right);
+
+        return max(left, right);
     }
     bool isBalanced(TreeNode* root) {
-        if(root==NULL){
+        if (root == NULL)
             return true;
-        }
+        int left_h = height(root->left);
+        int right_h = height(root->right);
 
-        int left=height(root->left);
-        int right=height(root->right);
-        bool diff = (abs(left-right)<=1);
-        
-        bool left_side=isBalanced(root->left);
-        bool right_side=isBalanced(root->right);
+        bool b1 = (abs(right_h - left_h) <= 1) ? true : false;
 
-        if(diff && left_side && right_side){
-            return true;
-        }
-        else{
-            return false;
-        }
+        bool b2 = isBalanced(root->left);
+        bool b3 = isBalanced(root->right);
 
+
+        return (b1 && b2 && b3);
     }
 };
