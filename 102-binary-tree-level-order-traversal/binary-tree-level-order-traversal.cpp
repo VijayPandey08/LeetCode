51226af3df3c821662c72dca18207130c2e28612
Vijay Pandey <vijaypandey0825@gmail.com>
2024-09-30 02:18:12 +0530

    Time: 7 ms (28.25%) | Memory: 15.1 MB (83.89%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,48 +1,54 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        void solve(TreeNode *root, vector<vector<int>> &ans)
-        {
-            if (root == NULL) return;
-            queue<TreeNode*> q;
-            q.push(root);
-            while (!q.empty())
-            {
-                int count = q.size();
-                vector<int> curr;
-                for (int i = 0; i < count; i++)
-                {
-                    TreeNode *node = q.front();
-                    q.pop();
-                    curr.push_back(node->val);
-                    if (node->left)
-                    {
-                        q.push(node->left);
-                    }
-                    if (node->right)
-                    {
-                        q.push(node->right);
-                    }
-                }
-                    ans.push_back(curr);
-            }
-        }
-    vector<vector < int>> levelOrder(TreeNode *root)
-    {
-        vector<vector < int>> ans;
+class Solution {
+public:
+    void solve(TreeNode* root, vector<vector<int>> &ans, vector<int> &temp) {
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
 
-        solve(root, ans);
+        // vector<int> temp;
+        while (!q.empty()) {
+            TreeNode* test = q.front();
+            q.pop();
+         if(test!=NULL){
+                temp.push_back(test->val);
+                if (test->left != NULL)
+                    q.push(test->left);
+                if (test->right != NULL)
+                    q.push(test->right);
+            
+         }
+         else if(test == NULL){
+                if (!q.empty()) {
+                    q.push(NULL);
+                    ans.push_back(temp);
+                    temp.erase(temp.begin(), temp.end());
+                } else {
+                    ans.push_back(temp);
+                    break;
+                }
+         }
+        }
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        if(root==NULL) return ans;
+
+        solve(root, ans, temp);
+
         return ans;
     }
 };
